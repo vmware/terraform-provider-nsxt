fa91acc65a59c6d270ab0eff735cafa06ce3c9c4
Chris Marchesi <chrism@vancluevertech.com>
2018-04-09 13:30:05 -0700

    Change import path to new provider location

--- main.go
+++ main.go
@@ -6,7 +6,7 @@ package main
 import (
 	"github.com/hashicorp/terraform/plugin"
 	"github.com/hashicorp/terraform/terraform"
-	"github.com/vmware/terraform-provider-nsxt/nsxt"
+	"github.com/terraform-providers/terraform-provider-nsxt/nsxt"
 )
 
 func main() {
