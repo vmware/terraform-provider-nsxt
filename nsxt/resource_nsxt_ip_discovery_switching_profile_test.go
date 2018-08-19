7b3284a1c3b6f5910a66b76222a3b5c8cc9d54c3
Adit Sarfaty <asarfaty@vmware.com>
2018-08-19 16:03:08 +0300

    Adding new switching profiles resources

---
+++ nsxt/resource_nsxt_ip_discovery_switching_profile_test.go
@@ -0,0 +1,156 @@
+/* Copyright © 2018 VMware, Inc. All Rights Reserved.
+   SPDX-License-Identifier: MPL-2.0 */
+
+package nsxt
+
+import (
+	"fmt"
+	"github.com/hashicorp/terraform/helper/resource"
+	"github.com/hashicorp/terraform/terraform"
+	"github.com/vmware/go-vmware-nsxt"
+	"net/http"
+	"testing"
+)
+
+func TestAccResourceNsxtIpDiscoverySwitchingProfile_basic(t *testing.T) {
+	name := "test-nsx-switching-profile"
+	updatedName := fmt.Sprintf("%s-update", name)
+	testResourceName := "nsxt_ip_discovery_switching_profile.test"
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:  func() { testAccPreCheck(t) },
+		Providers: testAccProviders,
+		CheckDestroy: func(state *terraform.State) error {
+			return testAccNSXIpDiscoverySwitchingProfileCheckDestroy(state, name)
+		},
+		Steps: []resource.TestStep{
+			{
+				Config: testAccNSXIpDiscoverySwitchingProfileCreateTemplate(name, "true", "true", "true", "2"),
+				Check: resource.ComposeTestCheckFunc(
+					testAccNSXIpDiscoverySwitchingProfileExists(name, testResourceName),
+					resource.TestCheckResourceAttr(testResourceName, "display_name", name),
+					resource.TestCheckResourceAttr(testResourceName, "description", "test description"),
+					resource.TestCheckResourceAttr(testResourceName, "vm_tools_enabled", "true"),
+					resource.TestCheckResourceAttr(testResourceName, "arp_snooping_enabled", "true"),
+					resource.TestCheckResourceAttr(testResourceName, "dhcp_snooping_enabled", "true"),
+					resource.TestCheckResourceAttr(testResourceName, "arp_bindings_limit", "2"),
+					resource.TestCheckResourceAttr(testResourceName, "tag.#", "1"),
+				),
+			},
+			{
+				Config: testAccNSXIpDiscoverySwitchingProfileCreateTemplate(updatedName, "false", "false", "false", "3"),
+				Check: resource.ComposeTestCheckFunc(
+					testAccNSXIpDiscoverySwitchingProfileExists(updatedName, testResourceName),
+					resource.TestCheckResourceAttr(testResourceName, "display_name", updatedName),
+					resource.TestCheckResourceAttr(testResourceName, "description", "test description"),
+					resource.TestCheckResourceAttr(testResourceName, "vm_tools_enabled", "false"),
+					resource.TestCheckResourceAttr(testResourceName, "arp_snooping_enabled", "false"),
+					resource.TestCheckResourceAttr(testResourceName, "dhcp_snooping_enabled", "false"),
+					resource.TestCheckResourceAttr(testResourceName, "arp_bindings_limit", "3"),
+					resource.TestCheckResourceAttr(testResourceName, "tag.#", "1"),
+				),
+			},
+		},
+	})
+}
+
+func TestAccResourceNsxtIpDiscoverySwitchingProfile_importBasic(t *testing.T) {
+	name := "test-nsx-application-profile"
+	testResourceName := "nsxt_ip_discovery_switching_profile.test"
+	resource.Test(t, resource.TestCase{
+		PreCheck:  func() { testAccPreCheck(t) },
+		Providers: testAccProviders,
+		CheckDestroy: func(state *terraform.State) error {
+			return testAccNSXIpDiscoverySwitchingProfileCheckDestroy(state, name)
+		},
+		Steps: []resource.TestStep{
+			{
+				Config: testAccNSXIpDiscoverySwitchingProfileCreateTemplateTrivial(name),
+			},
+			{
+				ResourceName:      testResourceName,
+				ImportState:       true,
+				ImportStateVerify: true,
+			},
+		},
+	})
+}
+
+func testAccNSXIpDiscoverySwitchingProfileExists(displayName string, resourceName string) resource.TestCheckFunc {
+	return func(state *terraform.State) error {
+		nsxClient := testAccProvider.Meta().(*nsxt.APIClient)
+		rs, ok := state.RootModule().Resources[resourceName]
+		if !ok {
+			return fmt.Errorf("NSX switching profile resource %s not found in resources", resourceName)
+		}
+
+		resourceID := rs.Primary.ID
+		if resourceID == "" {
+			return fmt.Errorf("NSX switching profile resource ID not set in resources ")
+		}
+
+		profile, responseCode, err := nsxClient.LogicalSwitchingApi.GetIpDiscoverySwitchingProfile(nsxClient.Context, resourceID)
+		if err != nil {
+			return fmt.Errorf("Error while retrieving switching profile with ID %s. Error: %v", resourceID, err)
+		}
+
+		if responseCode.StatusCode != http.StatusOK {
+			return fmt.Errorf("Error while checking if switching profile %s exists. HTTP return code was %d", resourceID, responseCode.StatusCode)
+		}
+
+		if displayName == profile.DisplayName {
+			return nil
+		}
+		return fmt.Errorf("NSX switching profile %s wasn't found", displayName)
+	}
+}
+
+func testAccNSXIpDiscoverySwitchingProfileCheckDestroy(state *terraform.State, displayName string) error {
+	nsxClient := testAccProvider.Meta().(*nsxt.APIClient)
+	for _, rs := range state.RootModule().Resources {
+
+		if rs.Type != "nsxt_ip_discovery_switching_profile" {
+			continue
+		}
+
+		resourceID := rs.Primary.Attributes["id"]
+		profile, responseCode, err := nsxClient.LogicalSwitchingApi.GetIpDiscoverySwitchingProfile(nsxClient.Context, resourceID)
+		if err != nil {
+			if responseCode.StatusCode != http.StatusOK {
+				return nil
+			}
+			return fmt.Errorf("Error while retrieving switching profile with ID %s. Error: %v", resourceID, err)
+		}
+
+		if displayName == profile.DisplayName {
+			return fmt.Errorf("NSX switching profile %s still exists", displayName)
+		}
+	}
+	return nil
+}
+
+func testAccNSXIpDiscoverySwitchingProfileCreateTemplate(name string, vmToolsEnabled string, arpSnoopingEnabled string, dhcpSnoopingEnabled string, arpBindingsLimit string) string {
+	return fmt.Sprintf(`
+resource "nsxt_ip_discovery_switching_profile" "test" {
+  display_name          = "%s"
+  description           = "test description"
+  vm_tools_enabled      = "%s"
+  arp_snooping_enabled  = "%s"
+  dhcp_snooping_enabled = "%s"
+  arp_bindings_limit    = "%s"
+
+  tag {
+    scope = "scope1"
+    tag   = "tag1"
+  }
+}
+`, name, vmToolsEnabled, arpSnoopingEnabled, dhcpSnoopingEnabled, arpBindingsLimit)
+}
+
+func testAccNSXIpDiscoverySwitchingProfileCreateTemplateTrivial(name string) string {
+	return fmt.Sprintf(`
+resource "nsxt_ip_discovery_switching_profile" "test" {
+  display_name          = "%s"
+}
+`, name)
+}
---
+++ nsxt/resource_nsxt_mac_management_switching_profile.go
@@ -0,0 +1,222 @@
+/* Copyright © 2018 VMware, Inc. All Rights Reserved.
+   SPDX-License-Identifier: MPL-2.0 */
+
+package nsxt
+
+import (
+	"fmt"
+	"github.com/hashicorp/terraform/helper/schema"
+	"github.com/hashicorp/terraform/helper/validation"
+	api "github.com/vmware/go-vmware-nsxt"
+	"github.com/vmware/go-vmware-nsxt/manager"
+	"log"
+	"net/http"
+)
+
+func resourceNsxtMacManagementSwitchingProfile() *schema.Resource {
+	return &schema.Resource{
+		Create: resourceNsxtMacManagementSwitchingProfileCreate,
+		Read:   resourceNsxtMacManagementSwitchingProfileRead,
+		Update: resourceNsxtMacManagementSwitchingProfileUpdate,
+		Delete: resourceNsxtMacManagementSwitchingProfileDelete,
+		Importer: &schema.ResourceImporter{
+			State: schema.ImportStatePassthrough,
+		},
+
+		Schema: map[string]*schema.Schema{
+			"revision": getRevisionSchema(),
+			"description": &schema.Schema{
+				Type:        schema.TypeString,
+				Description: "Description of this resource",
+				Optional:    true,
+			},
+			"display_name": &schema.Schema{
+				Type:        schema.TypeString,
+				Description: "The display name of this resource. Defaults to ID if not set",
+				Optional:    true,
+				Computed:    true,
+			},
+			"tag": getTagsSchema(),
+			"mac_change_allowed": &schema.Schema{
+				Type:        schema.TypeBool,
+				Description: "Allowing source MAC address change",
+				Optional:    true,
+				Default:     false,
+			},
+			"mac_learning": &schema.Schema{
+				Type:     schema.TypeList,
+				MaxItems: 1,
+				Optional: true,
+				Elem: &schema.Resource{
+					Schema: map[string]*schema.Schema{
+						"enabled": &schema.Schema{
+							Type:        schema.TypeBool,
+							Description: "Allowing source MAC address learning",
+							Optional:    true,
+							Default:     false,
+						},
+						"unicast_flooding_allowed": &schema.Schema{
+							Type:        schema.TypeBool,
+							Description: "Allowing flooding for unlearned MAC for ingress traffic",
+							Optional:    true,
+							Default:     false,
+						},
+						"limit": &schema.Schema{
+							Type:         schema.TypeInt,
+							Description:  "The maximum number of MAC addresses that can be learned on this port",
+							Optional:     true,
+							Default:      4096,
+							ValidateFunc: validation.IntAtLeast(1),
+						},
+						"limit_policy": &schema.Schema{
+							Type:         schema.TypeString,
+							Description:  "The policy after MAC Limit is exceeded",
+							Optional:     true,
+							Default:      "ALLOW",
+							ValidateFunc: validation.StringInSlice([]string{"ALLOW", "DROP"}, false),
+						},
+					},
+				},
+			},
+		},
+	}
+}
+
+func getMacLearningFromSchema(d *schema.ResourceData) *manager.MacLearningSpec {
+	macLearningConfs := d.Get("mac_learning").([]interface{})
+	for _, macLearningConf := range macLearningConfs {
+		// only 1 is allowed
+		data := macLearningConf.(map[string]interface{})
+		return &manager.MacLearningSpec{
+			Enabled:                data["enabled"].(bool),
+			UnicastFloodingAllowed: data["unicast_flooding_allowed"].(bool),
+			Limit:       int32(data["limit"].(int)),
+			LimitPolicy: data["limit_policy"].(string),
+		}
+	}
+
+	return nil
+}
+
+func setMacLearningInSchema(d *schema.ResourceData, macLearning *manager.MacLearningSpec) error {
+	var resultList []map[string]interface{}
+	if macLearning != nil {
+		elem := make(map[string]interface{})
+		elem["enabled"] = macLearning.Enabled
+		elem["unicast_flooding_allowed"] = macLearning.UnicastFloodingAllowed
+		elem["limit"] = macLearning.Limit
+		elem["limit_policy"] = macLearning.LimitPolicy
+		resultList = append(resultList, elem)
+	}
+	err := d.Set("mac_learning", resultList)
+	return err
+}
+
+func resourceNsxtMacManagementSwitchingProfileCreate(d *schema.ResourceData, m interface{}) error {
+	nsxClient := m.(*api.APIClient)
+	description := d.Get("description").(string)
+	displayName := d.Get("display_name").(string)
+	tags := getTagsFromSchema(d)
+	macChangeAllowed := d.Get("mac_change_allowed").(bool)
+	macLearning := getMacLearningFromSchema(d)
+
+	switchingProfile := manager.MacManagementSwitchingProfile{
+		Description:      description,
+		DisplayName:      displayName,
+		Tags:             tags,
+		MacChangeAllowed: macChangeAllowed,
+		MacLearning:      macLearning,
+	}
+
+	switchingProfile, resp, err := nsxClient.LogicalSwitchingApi.CreateMacManagementSwitchingProfile(nsxClient.Context, switchingProfile)
+
+	if err != nil {
+		return fmt.Errorf("Error during MacManagementSwitchingProfile create: %v", err)
+	}
+
+	if resp.StatusCode != http.StatusCreated {
+		return fmt.Errorf("Unexpected status returned during MacManagementSwitchingProfile create: %v", resp.StatusCode)
+	}
+	d.SetId(switchingProfile.Id)
+
+	return resourceNsxtMacManagementSwitchingProfileRead(d, m)
+}
+
+func resourceNsxtMacManagementSwitchingProfileRead(d *schema.ResourceData, m interface{}) error {
+	nsxClient := m.(*api.APIClient)
+	id := d.Id()
+	if id == "" {
+		return fmt.Errorf("Error obtaining logical object id")
+	}
+
+	switchingProfile, resp, err := nsxClient.LogicalSwitchingApi.GetMacManagementSwitchingProfile(nsxClient.Context, id)
+	if err != nil {
+		return fmt.Errorf("Error during MacManagementSwitchingProfile read: %v", err)
+	}
+
+	if resp.StatusCode == http.StatusNotFound {
+		log.Printf("[DEBUG] MacManagementSwitchingProfile %s not found", id)
+		d.SetId("")
+		return nil
+	}
+	d.Set("revision", switchingProfile.Revision)
+	d.Set("description", switchingProfile.Description)
+	d.Set("display_name", switchingProfile.DisplayName)
+	d.Set("mac_change_allowed", switchingProfile.MacChangeAllowed)
+	setTagsInSchema(d, switchingProfile.Tags)
+	err = setMacLearningInSchema(d, switchingProfile.MacLearning)
+	if err != nil {
+		return fmt.Errorf("Error during setting MacManagementSwitchingProfile MacLearning: %v", err)
+	}
+
+	return nil
+}
+
+func resourceNsxtMacManagementSwitchingProfileUpdate(d *schema.ResourceData, m interface{}) error {
+	nsxClient := m.(*api.APIClient)
+	id := d.Id()
+	if id == "" {
+		return fmt.Errorf("Error obtaining logical object id")
+	}
+
+	description := d.Get("description").(string)
+	displayName := d.Get("display_name").(string)
+	tags := getTagsFromSchema(d)
+	macChangeAllowed := d.Get("mac_change_allowed").(bool)
+	macLearning := getMacLearningFromSchema(d)
+
+	switchingProfile := manager.MacManagementSwitchingProfile{
+		Description:      description,
+		DisplayName:      displayName,
+		Tags:             tags,
+		MacChangeAllowed: macChangeAllowed,
+		MacLearning:      macLearning,
+	}
+
+	switchingProfile, resp, err := nsxClient.LogicalSwitchingApi.UpdateMacManagementSwitchingProfile(nsxClient.Context, id, switchingProfile)
+
+	if err != nil || resp.StatusCode == http.StatusNotFound {
+		return fmt.Errorf("Error during MacManagementSwitchingProfile update: %v", err)
+	}
+
+	return resourceNsxtMacManagementSwitchingProfileRead(d, m)
+}
+
+func resourceNsxtMacManagementSwitchingProfileDelete(d *schema.ResourceData, m interface{}) error {
+	nsxClient := m.(*api.APIClient)
+	id := d.Id()
+	if id == "" {
+		return fmt.Errorf("Error obtaining logical object id")
+	}
+
+	resp, err := nsxClient.LogicalSwitchingApi.DeleteSwitchingProfile(nsxClient.Context, id, nil)
+	if err != nil {
+		return fmt.Errorf("Error during MacManagementSwitchingProfile delete: %v", err)
+	}
+
+	if resp.StatusCode == http.StatusNotFound {
+		log.Printf("[DEBUG] MacManagementSwitchingProfile %s not found", id)
+		d.SetId("")
+	}
+	return nil
+}
