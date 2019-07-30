883d8ee240592cb7f431872c5b955e7e19f03e4d
Anna Khmelnitsky <akhmelnitsky@vmware.com>
2019-07-30 16:29:19 -0700

    DNM: test

--- nsxt/resource_nsxt_qos_switching_profile.go
+++ nsxt/resource_nsxt_qos_switching_profile.go
@@ -21,6 +21,7 @@ var rateShaperSchemaNames = []string{"ingress_rate_shaper", "ingress_broadcast_r
 var rateShaperScales = []string{"mbps", "kbps", "mbps"}
 var rateShaperResourceTypes = []string{"IngressRateShaper", "IngressBroadcastRateShaper", "EgressRateShaper"}
 
+
 func resourceNsxtQosSwitchingProfile() *schema.Resource {
 	return &schema.Resource{
 		Create: resourceNsxtQosSwitchingProfileCreate,
