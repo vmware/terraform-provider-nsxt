e452567a0d62e997fd647d08f974f96b8f460373
asarfaty <asarfaty@vmware.com>
2020-06-18 12:14:01 +0200

    Add missing sdk files

---
+++ vendor/github.com/vmware/vsphere-automation-sdk-go/services/nsxt-gm/global_infra/tier_0s/locale_services/BgpTypes.go
@@ -0,0 +1,208 @@
+/* Copyright © 2019 VMware, Inc. All Rights Reserved.
+   SPDX-License-Identifier: BSD-2-Clause */
+
+// Code generated. DO NOT EDIT.
+
+/*
+ * Data type definitions file for service: Bgp.
+ * Includes binding types of a structures and enumerations defined in the service.
+ * Shared by client-side stubs and server-side skeletons to ensure type
+ * compatibility.
+ */
+
+package locale_services
+
+import (
+	"reflect"
+	"github.com/vmware/vsphere-automation-sdk-go/services/nsxt-gm/model"
+	"github.com/vmware/vsphere-automation-sdk-go/runtime/bindings"
+	"github.com/vmware/vsphere-automation-sdk-go/runtime/data"
+	"github.com/vmware/vsphere-automation-sdk-go/runtime/protocol"
+)
+
+
+
+
+
+func bgpGetInputType() bindings.StructType {
+	fields := make(map[string]bindings.BindingType)
+	fieldNameMap := make(map[string]string)
+	fields["tier0_id"] = bindings.NewStringType()
+	fields["locale_service_id"] = bindings.NewStringType()
+	fieldNameMap["tier0_id"] = "Tier0Id"
+	fieldNameMap["locale_service_id"] = "LocaleServiceId"
+	var validators = []bindings.Validator{}
+	return bindings.NewStructType("operation-input", fields, reflect.TypeOf(data.StructValue{}), fieldNameMap, validators)
+}
+
+func bgpGetOutputType() bindings.BindingType {
+	return bindings.NewReferenceType(model.BgpRoutingConfigBindingType)
+}
+
+func bgpGetRestMetadata() protocol.OperationRestMetadata {
+	fields := map[string]bindings.BindingType{}
+	fieldNameMap := map[string]string{}
+	paramsTypeMap := map[string]bindings.BindingType{}
+	pathParams := map[string]string{}
+	queryParams := map[string]string{}
+	headerParams := map[string]string{}
+	dispatchHeaderParams := map[string]string{}
+	bodyFieldsMap := map[string]string{}
+	fields["tier0_id"] = bindings.NewStringType()
+	fields["locale_service_id"] = bindings.NewStringType()
+	fieldNameMap["tier0_id"] = "Tier0Id"
+	fieldNameMap["locale_service_id"] = "LocaleServiceId"
+	paramsTypeMap["tier0_id"] = bindings.NewStringType()
+	paramsTypeMap["locale_service_id"] = bindings.NewStringType()
+	paramsTypeMap["tier0Id"] = bindings.NewStringType()
+	paramsTypeMap["localeServiceId"] = bindings.NewStringType()
+	pathParams["tier0_id"] = "tier0Id"
+	pathParams["locale_service_id"] = "localeServiceId"
+	resultHeaders := map[string]string{}
+	errorHeaders := map[string]map[string]string{}
+	return protocol.NewOperationRestMetadata(
+		fields,
+		fieldNameMap,
+		paramsTypeMap,
+		pathParams,
+		queryParams,
+		headerParams,
+		dispatchHeaderParams,
+		bodyFieldsMap,
+		"",
+		"",
+		"GET",
+		"/global-manager/api/v1/global-infra/tier-0s/{tier0Id}/locale-services/{localeServiceId}/bgp",
+		"",
+		resultHeaders,
+		200,
+		"",
+		errorHeaders,
+		map[string]int{"com.vmware.vapi.std.errors.invalid_request": 400,"com.vmware.vapi.std.errors.unauthorized": 403,"com.vmware.vapi.std.errors.service_unavailable": 503,"com.vmware.vapi.std.errors.internal_server_error": 500,"com.vmware.vapi.std.errors.not_found": 404})
+}
+
+func bgpPatchInputType() bindings.StructType {
+	fields := make(map[string]bindings.BindingType)
+	fieldNameMap := make(map[string]string)
+	fields["tier0_id"] = bindings.NewStringType()
+	fields["locale_service_id"] = bindings.NewStringType()
+	fields["bgp_routing_config"] = bindings.NewReferenceType(model.BgpRoutingConfigBindingType)
+	fieldNameMap["tier0_id"] = "Tier0Id"
+	fieldNameMap["locale_service_id"] = "LocaleServiceId"
+	fieldNameMap["bgp_routing_config"] = "BgpRoutingConfig"
+	var validators = []bindings.Validator{}
+	return bindings.NewStructType("operation-input", fields, reflect.TypeOf(data.StructValue{}), fieldNameMap, validators)
+}
+
+func bgpPatchOutputType() bindings.BindingType {
+	return bindings.NewVoidType()
+}
+
+func bgpPatchRestMetadata() protocol.OperationRestMetadata {
+	fields := map[string]bindings.BindingType{}
+	fieldNameMap := map[string]string{}
+	paramsTypeMap := map[string]bindings.BindingType{}
+	pathParams := map[string]string{}
+	queryParams := map[string]string{}
+	headerParams := map[string]string{}
+	dispatchHeaderParams := map[string]string{}
+	bodyFieldsMap := map[string]string{}
+	fields["tier0_id"] = bindings.NewStringType()
+	fields["locale_service_id"] = bindings.NewStringType()
+	fields["bgp_routing_config"] = bindings.NewReferenceType(model.BgpRoutingConfigBindingType)
+	fieldNameMap["tier0_id"] = "Tier0Id"
+	fieldNameMap["locale_service_id"] = "LocaleServiceId"
+	fieldNameMap["bgp_routing_config"] = "BgpRoutingConfig"
+	paramsTypeMap["tier0_id"] = bindings.NewStringType()
+	paramsTypeMap["locale_service_id"] = bindings.NewStringType()
+	paramsTypeMap["bgp_routing_config"] = bindings.NewReferenceType(model.BgpRoutingConfigBindingType)
+	paramsTypeMap["tier0Id"] = bindings.NewStringType()
+	paramsTypeMap["localeServiceId"] = bindings.NewStringType()
+	pathParams["tier0_id"] = "tier0Id"
+	pathParams["locale_service_id"] = "localeServiceId"
+	resultHeaders := map[string]string{}
+	errorHeaders := map[string]map[string]string{}
+	return protocol.NewOperationRestMetadata(
+		fields,
+		fieldNameMap,
+		paramsTypeMap,
+		pathParams,
+		queryParams,
+		headerParams,
+		dispatchHeaderParams,
+		bodyFieldsMap,
+		"",
+		"bgp_routing_config",
+		"PATCH",
+		"/global-manager/api/v1/global-infra/tier-0s/{tier0Id}/locale-services/{localeServiceId}/bgp",
+		"",
+		resultHeaders,
+		204,
+		"",
+		errorHeaders,
+		map[string]int{"com.vmware.vapi.std.errors.invalid_request": 400,"com.vmware.vapi.std.errors.unauthorized": 403,"com.vmware.vapi.std.errors.service_unavailable": 503,"com.vmware.vapi.std.errors.internal_server_error": 500,"com.vmware.vapi.std.errors.not_found": 404})
+}
+
+func bgpUpdateInputType() bindings.StructType {
+	fields := make(map[string]bindings.BindingType)
+	fieldNameMap := make(map[string]string)
+	fields["tier0_id"] = bindings.NewStringType()
+	fields["locale_service_id"] = bindings.NewStringType()
+	fields["bgp_routing_config"] = bindings.NewReferenceType(model.BgpRoutingConfigBindingType)
+	fieldNameMap["tier0_id"] = "Tier0Id"
+	fieldNameMap["locale_service_id"] = "LocaleServiceId"
+	fieldNameMap["bgp_routing_config"] = "BgpRoutingConfig"
+	var validators = []bindings.Validator{}
+	return bindings.NewStructType("operation-input", fields, reflect.TypeOf(data.StructValue{}), fieldNameMap, validators)
+}
+
+func bgpUpdateOutputType() bindings.BindingType {
+	return bindings.NewReferenceType(model.BgpRoutingConfigBindingType)
+}
+
+func bgpUpdateRestMetadata() protocol.OperationRestMetadata {
+	fields := map[string]bindings.BindingType{}
+	fieldNameMap := map[string]string{}
+	paramsTypeMap := map[string]bindings.BindingType{}
+	pathParams := map[string]string{}
+	queryParams := map[string]string{}
+	headerParams := map[string]string{}
+	dispatchHeaderParams := map[string]string{}
+	bodyFieldsMap := map[string]string{}
+	fields["tier0_id"] = bindings.NewStringType()
+	fields["locale_service_id"] = bindings.NewStringType()
+	fields["bgp_routing_config"] = bindings.NewReferenceType(model.BgpRoutingConfigBindingType)
+	fieldNameMap["tier0_id"] = "Tier0Id"
+	fieldNameMap["locale_service_id"] = "LocaleServiceId"
+	fieldNameMap["bgp_routing_config"] = "BgpRoutingConfig"
+	paramsTypeMap["tier0_id"] = bindings.NewStringType()
+	paramsTypeMap["locale_service_id"] = bindings.NewStringType()
+	paramsTypeMap["bgp_routing_config"] = bindings.NewReferenceType(model.BgpRoutingConfigBindingType)
+	paramsTypeMap["tier0Id"] = bindings.NewStringType()
+	paramsTypeMap["localeServiceId"] = bindings.NewStringType()
+	pathParams["tier0_id"] = "tier0Id"
+	pathParams["locale_service_id"] = "localeServiceId"
+	resultHeaders := map[string]string{}
+	errorHeaders := map[string]map[string]string{}
+	return protocol.NewOperationRestMetadata(
+		fields,
+		fieldNameMap,
+		paramsTypeMap,
+		pathParams,
+		queryParams,
+		headerParams,
+		dispatchHeaderParams,
+		bodyFieldsMap,
+		"",
+		"bgp_routing_config",
+		"PUT",
+		"/global-manager/api/v1/global-infra/tier-0s/{tier0Id}/locale-services/{localeServiceId}/bgp",
+		"",
+		resultHeaders,
+		200,
+		"",
+		errorHeaders,
+		map[string]int{"com.vmware.vapi.std.errors.invalid_request": 400,"com.vmware.vapi.std.errors.unauthorized": 403,"com.vmware.vapi.std.errors.service_unavailable": 503,"com.vmware.vapi.std.errors.internal_server_error": 500,"com.vmware.vapi.std.errors.not_found": 404})
+}
+
+
---
+++ vendor/github.com/vmware/vsphere-automation-sdk-go/services/nsxt-gm/global_infra/tier_0s/locale_services/Tier0DeploymentMapsClient.go
@@ -0,0 +1,89 @@
+/* Copyright © 2019 VMware, Inc. All Rights Reserved.
+   SPDX-License-Identifier: BSD-2-Clause */
+
+// Code generated. DO NOT EDIT.
+
+/*
+ * Interface file for service: Tier0DeploymentMaps
+ * Used by client-side stubs.
+ */
+
+package locale_services
+
+import (
+	"github.com/vmware/vsphere-automation-sdk-go/services/nsxt-gm/model"
+)
+
+type Tier0DeploymentMapsClient interface {
+
+    // Delete Tier-0 Deployment Map
+    //
+    // @param tier0IdParam (required)
+    // @param localeServiceIdParam (required)
+    // @param tier0DeploymentMapIdParam (required)
+    // @throws InvalidRequest  Bad Request, Precondition Failed
+    // @throws Unauthorized  Forbidden
+    // @throws ServiceUnavailable  Service Unavailable
+    // @throws InternalServerError  Internal Server Error
+    // @throws NotFound  Not Found
+	Delete(tier0IdParam string, localeServiceIdParam string, tier0DeploymentMapIdParam string) error
+
+    // Read a Tier-0 Deployment Map
+    //
+    // @param tier0IdParam (required)
+    // @param localeServiceIdParam (required)
+    // @param tier0DeploymentMapIdParam (required)
+    // @return com.vmware.nsx_global_policy.model.Tier0DeploymentMap
+    // @throws InvalidRequest  Bad Request, Precondition Failed
+    // @throws Unauthorized  Forbidden
+    // @throws ServiceUnavailable  Service Unavailable
+    // @throws InternalServerError  Internal Server Error
+    // @throws NotFound  Not Found
+	Get(tier0IdParam string, localeServiceIdParam string, tier0DeploymentMapIdParam string) (model.Tier0DeploymentMap, error)
+
+    // Paginated list of all Tier-0 Deployment Entries.
+    //
+    // @param tier0IdParam (required)
+    // @param localeServiceIdParam (required)
+    // @param cursorParam Opaque cursor to be used for getting next page of records (supplied by current result page) (optional)
+    // @param includeMarkForDeleteObjectsParam Include objects that are marked for deletion in results (optional, default to false)
+    // @param includedFieldsParam Comma separated list of fields that should be included in query result (optional)
+    // @param pageSizeParam Maximum number of results to return in this page (server may return fewer) (optional, default to 1000)
+    // @param sortAscendingParam (optional)
+    // @param sortByParam Field by which records are sorted (optional)
+    // @return com.vmware.nsx_global_policy.model.Tier0DeploymentMapListResult
+    // @throws InvalidRequest  Bad Request, Precondition Failed
+    // @throws Unauthorized  Forbidden
+    // @throws ServiceUnavailable  Service Unavailable
+    // @throws InternalServerError  Internal Server Error
+    // @throws NotFound  Not Found
+	List(tier0IdParam string, localeServiceIdParam string, cursorParam *string, includeMarkForDeleteObjectsParam *bool, includedFieldsParam *string, pageSizeParam *int64, sortAscendingParam *bool, sortByParam *string) (model.Tier0DeploymentMapListResult, error)
+
+    // If the passed Tier-0 Deployment Map does not already exist, create a new Tier-0 Deployment Map. If it already exists, patch it.
+    //
+    // @param tier0IdParam (required)
+    // @param localeServiceIdParam (required)
+    // @param tier0DeploymentMapIdParam (required)
+    // @param tier0DeploymentMapParam (required)
+    // @return com.vmware.nsx_global_policy.model.Tier0DeploymentMap
+    // @throws InvalidRequest  Bad Request, Precondition Failed
+    // @throws Unauthorized  Forbidden
+    // @throws ServiceUnavailable  Service Unavailable
+    // @throws InternalServerError  Internal Server Error
+    // @throws NotFound  Not Found
+	Patch(tier0IdParam string, localeServiceIdParam string, tier0DeploymentMapIdParam string, tier0DeploymentMapParam model.Tier0DeploymentMap) (model.Tier0DeploymentMap, error)
+
+    // If the passed Tier-0 Deployment Map does not already exist, create a new Tier-0 Deployment Map. If it already exists, replace it.
+    //
+    // @param tier0IdParam (required)
+    // @param localeServiceIdParam (required)
+    // @param tier0DeploymentMapIdParam (required)
+    // @param tier0DeploymentMapParam (required)
+    // @return com.vmware.nsx_global_policy.model.Tier0DeploymentMap
+    // @throws InvalidRequest  Bad Request, Precondition Failed
+    // @throws Unauthorized  Forbidden
+    // @throws ServiceUnavailable  Service Unavailable
+    // @throws InternalServerError  Internal Server Error
+    // @throws NotFound  Not Found
+	Update(tier0IdParam string, localeServiceIdParam string, tier0DeploymentMapIdParam string, tier0DeploymentMapParam model.Tier0DeploymentMap) (model.Tier0DeploymentMap, error)
+}
