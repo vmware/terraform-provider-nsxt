249a460ddc365e976b861388baeda632f8027fa8
asarfaty <asarfaty@vmware.com>
2021-01-13 12:42:02 +0200

    Support policy qos profile as a resource

---
+++ nsxt/resource_nsxt_policy_qos_profile.go
@@ -0,0 +1,352 @@
+/* Copyright © 2020 VMware, Inc. All Rights Reserved.
+   SPDX-License-Identifier: MPL-2.0 */
+
+package nsxt
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/validation"
+	"github.com/vmware/vsphere-automation-sdk-go/runtime/bindings"
+	"github.com/vmware/vsphere-automation-sdk-go/runtime/data"
+	"github.com/vmware/vsphere-automation-sdk-go/runtime/protocol/client"
+	gm_infra "github.com/vmware/vsphere-automation-sdk-go/services/nsxt-gm/global_infra"
+	gm_model "github.com/vmware/vsphere-automation-sdk-go/services/nsxt-gm/model"
+	"github.com/vmware/vsphere-automation-sdk-go/services/nsxt/infra"
+	"github.com/vmware/vsphere-automation-sdk-go/services/nsxt/model"
+)
+
+var rateLimiterResourceTypes = []string{"IngressRateLimiter", "IngressBroadcastRateLimiter", "EgressRateLimiter"}
+
+func resourceNsxtPolicyQosProfile() *schema.Resource {
+	return &schema.Resource{
+		Create: resourceNsxtPolicyQosProfileCreate,
+		Read:   resourceNsxtPolicyQosProfileRead,
+		Update: resourceNsxtPolicyQosProfileUpdate,
+		Delete: resourceNsxtPolicyQosProfileDelete,
+		Importer: &schema.ResourceImporter{
+			State: schema.ImportStatePassthrough,
+		},
+
+		Schema: map[string]*schema.Schema{
+			"nsx_id":       getNsxIDSchema(),
+			"path":         getPathSchema(),
+			"display_name": getDisplayNameSchema(),
+			"description":  getDescriptionSchema(),
+			"revision":     getRevisionSchema(),
+			"tag":          getTagsSchema(),
+			"class_of_service": {
+				Type:         schema.TypeInt,
+				Description:  "Class of service",
+				Optional:     true,
+				ValidateFunc: validation.IntBetween(0, 7),
+			},
+			"dscp_trusted": {
+				Type:        schema.TypeBool,
+				Description: "Trust mode for DSCP",
+				Optional:    true,
+				Default:     false,
+			},
+
+			"dscp_priority": {
+				Type:         schema.TypeInt,
+				Description:  "DSCP Priority",
+				Optional:     true,
+				ValidateFunc: validation.IntBetween(0, 63),
+			},
+			"ingress_rate_shaper":           getQosRateShaperSchema(ingressRateShaperIndex),
+			"ingress_broadcast_rate_shaper": getQosRateShaperSchema(ingressBroadcastRateShaperIndex),
+			"egress_rate_shaper":            getQosRateShaperSchema(egressRateShaperIndex),
+		},
+	}
+}
+
+func resourceNsxtPolicyQosProfileExists(id string, connector *client.RestConnector, isGlobalmodel bool) (bool, error) {
+	var err error
+	if isGlobalmodel {
+		client := gm_infra.NewDefaultQosProfilesClient(connector)
+		_, err = client.Get(id)
+	} else {
+		client := infra.NewDefaultQosProfilesClient(connector)
+		_, err = client.Get(id)
+	}
+	if err == nil {
+		return true, nil
+	}
+
+	if isNotFoundError(err) {
+		return false, nil
+	}
+
+	return false, logAPIError("Error retrieving resource", err)
+}
+
+func getPolicyQosRateShaperFromSchema(d *schema.ResourceData, index int) *data.StructValue {
+	scale := rateShaperScales[index]
+	schemaName := rateShaperSchemaNames[index]
+	resourceType := rateLimiterResourceTypes[index]
+	shaperConfs := d.Get(schemaName).([]interface{})
+	for _, shaperConf := range shaperConfs {
+		// only 1 is allowed for each type
+		shaperData := shaperConf.(map[string]interface{})
+		enabled := shaperData["enabled"].(bool)
+		averageBW := int64(shaperData[fmt.Sprintf("average_bw_%s", scale)].(int))
+		burstSize := int64(shaperData["burst_size"].(int))
+		peakBW := int64(shaperData[fmt.Sprintf("peak_bw_%s", scale)].(int))
+
+		shaper := model.IngressRateLimiter{
+			ResourceType:    resourceType,
+			Enabled:         &enabled,
+			BurstSize:       &burstSize,
+			AverageBandwidth:&averageBW,
+			PeakBandwidth:   &peakBW,
+		}
+		converter := bindings.NewTypeConverter()
+		converter.SetMode(bindings.REST)
+		dataValue, _ := converter.ConvertToVapi(shaper, model.IngressRateLimiterBindingType())
+		return dataValue.(*data.StructValue)
+	}
+
+	return nil
+}
+
+func setPolicyQosRateShaperInSchema(d *schema.ResourceData, shaperConf []*data.StructValue, index int) {
+	scale := rateShaperScales[index]
+	schemaName := rateShaperSchemaNames[index]
+	resourceType := rateLimiterResourceTypes[index]
+	var shapers []map[string]interface{}
+	converter := bindings.NewTypeConverter()
+	converter.SetMode(bindings.REST)
+	for _, dataShaper := range shaperConf {
+		dataValue, _ := converter.ConvertToGolang(dataShaper, model.IngressRateLimiterBindingType())
+		shaper := dataValue.(model.IngressRateLimiter)
+		if shaper.ResourceType == resourceType {
+			average := *shaper.AverageBandwidth
+			peak := *shaper.PeakBandwidth
+			burst := *shaper.BurstSize
+			// Do not define schema for default shaper to avoid non-empty plan
+			if !*shaper.Enabled && (average+burst+peak == 0) {
+				return
+			}
+			elem := make(map[string]interface{})
+			elem["enabled"] = *shaper.Enabled
+			elem["burst_size"] = burst
+			elem[fmt.Sprintf("average_bw_%s", scale)] = average
+			elem[fmt.Sprintf("peak_bw_%s", scale)] = peak
+
+			shapers = append(shapers, elem)
+			err := d.Set(schemaName, shapers)
+			if err != nil {
+				log.Printf("[WARNING] Failed to set shapers in schema: %v", err)
+			}
+		}
+	}
+}
+
+func resourceNsxtPolicyQosProfileCreate(d *schema.ResourceData, m interface{}) error {
+	connector := getPolicyConnector(m)
+
+	// Initialize resource Id and verify this ID is not yet used
+	id, err := getOrGenerateID(d, m, resourceNsxtPolicyQosProfileExists)
+	if err != nil {
+		return err
+	}
+
+	displayName := d.Get("display_name").(string)
+	description := d.Get("description").(string)
+	tags := getPolicyTagsFromSchema(d)
+
+	classOfService := int64(d.Get("class_of_service").(int))
+	dscpTrusted := "UNTRUSTED"
+	if d.Get("dscp_trusted").(bool) {
+		dscpTrusted = "TRUSTED"
+	}
+	dscpPriority := int64(d.Get("dscp_priority").(int))
+	var shapers []*data.StructValue
+	for index := ingressRateShaperIndex; index <= egressRateShaperIndex; index++ {
+
+		shaper := getPolicyQosRateShaperFromSchema(d, index)
+		if shaper != nil {
+			shapers = append(shapers, shaper)
+		}
+	}
+
+	obj := model.QosProfile{
+		DisplayName:     &displayName,
+		Description:     &description,
+		Tags:            tags,
+		ClassOfService:  &classOfService,
+		Dscp: &model.QosDscp{
+			Mode:     &dscpTrusted,
+			Priority: &dscpPriority,
+		},
+		ShaperConfigurations: shapers,
+	}
+
+	// Create the resource using PATCH
+	log.Printf("[INFO] Creating QosProfile with ID %s", id)
+	boolFalse := false
+	if isPolicyGlobalManager(m) {
+		gmObj, err1 := convertModelBindingType(obj, model.QosProfileBindingType(), gm_model.QosProfileBindingType())
+		if err1 != nil {
+			return err1
+		}
+
+		client := gm_infra.NewDefaultQosProfilesClient(connector)
+		err = client.Patch(id, gmObj.(gm_model.QosProfile), &boolFalse)
+	} else {
+		client := infra.NewDefaultQosProfilesClient(connector)
+		err = client.Patch(id, obj, &boolFalse)
+	}
+	if err != nil {
+		return handleCreateError("QosProfile", id, err)
+	}
+
+	d.SetId(id)
+	d.Set("nsx_id", id)
+
+	return resourceNsxtPolicyQosProfileRead(d, m)
+}
+
+func resourceNsxtPolicyQosProfileRead(d *schema.ResourceData, m interface{}) error {
+	connector := getPolicyConnector(m)
+
+	id := d.Id()
+	if id == "" {
+		return fmt.Errorf("Error obtaining QosProfile ID")
+	}
+
+	var obj model.QosProfile
+	if isPolicyGlobalManager(m) {
+		client := gm_infra.NewDefaultQosProfilesClient(connector)
+		gmObj, err := client.Get(id)
+		if err != nil {
+			return handleReadError(d, "QosProfile", id, err)
+		}
+		rawObj, err := convertModelBindingType(gmObj, gm_model.QosProfileBindingType(), model.QosProfileBindingType())
+		if err != nil {
+			return err
+		}
+		obj = rawObj.(model.QosProfile)
+
+	} else {
+		var err error
+		client := infra.NewDefaultQosProfilesClient(connector)
+		obj, err = client.Get(id)
+		if err != nil {
+			return handleReadError(d, "QosProfile", id, err)
+		}
+	}
+
+	d.Set("display_name", obj.DisplayName)
+	d.Set("description", obj.Description)
+	setPolicyTagsInSchema(d, obj.Tags)
+	d.Set("nsx_id", id)
+	d.Set("path", obj.Path)
+	d.Set("revision", obj.Revision)
+	d.Set("class_of_service", obj.ClassOfService)
+	if *obj.Dscp.Mode == "TRUSTED" {
+		d.Set("dscp_trusted", true)
+	} else {
+		d.Set("dscp_trusted", false)
+	}
+	d.Set("dscp_priority", obj.Dscp.Priority)
+	for index := ingressRateShaperIndex; index <= egressRateShaperIndex; index++ {
+		setPolicyQosRateShaperInSchema(d, obj.ShaperConfigurations, index)
+	}
+
+	return nil
+}
+
+func resourceNsxtPolicyQosProfileUpdate(d *schema.ResourceData, m interface{}) error {
+	connector := getPolicyConnector(m)
+	client := infra.NewDefaultQosProfilesClient(connector)
+	if client == nil {
+		return policyResourceNotSupportedError()
+	}
+
+	id := d.Id()
+	if id == "" {
+		return fmt.Errorf("Error obtaining QosProfile ID")
+	}
+
+	// Read the rest of the configured parameters
+	description := d.Get("description").(string)
+	displayName := d.Get("display_name").(string)
+	tags := getPolicyTagsFromSchema(d)
+
+	classOfService := int64(d.Get("class_of_service").(int))
+	dscpTrusted := "UNTRUSTED"
+	if d.Get("dscp_trusted").(bool) {
+		dscpTrusted = "TRUSTED"
+	}
+	dscpPriority := int64(d.Get("dscp_priority").(int))
+	var shapers []*data.StructValue
+	for index := ingressRateShaperIndex; index <= egressRateShaperIndex; index++ {
+
+		shaper := getPolicyQosRateShaperFromSchema(d, index)
+		if shaper != nil {
+			shapers = append(shapers, shaper)
+		}
+	}
+
+	obj := model.QosProfile{
+		DisplayName:     &displayName,
+		Description:     &description,
+		Tags:            tags,
+		ClassOfService:  &classOfService,
+		Dscp: &model.QosDscp{
+			Mode:     &dscpTrusted,
+			Priority: &dscpPriority,
+		},
+		ShaperConfigurations: shapers,
+	}
+
+	// Create the resource using PATCH
+	log.Printf("[INFO] Updating QosProfile with ID %s", id)
+	boolFalse := false
+	var err error
+	if isPolicyGlobalManager(m) {
+		gmObj, err1 := convertModelBindingType(obj, model.QosProfileBindingType(), gm_model.QosProfileBindingType())
+		if err1 != nil {
+			return err1
+		}
+
+		client := gm_infra.NewDefaultQosProfilesClient(connector)
+		err = client.Patch(id, gmObj.(gm_model.QosProfile), &boolFalse)
+	} else {
+		client := infra.NewDefaultQosProfilesClient(connector)
+		err = client.Patch(id, obj, &boolFalse)
+	}
+	if err != nil {
+		return handleUpdateError("QosProfile", id, err)
+	}
+
+	return resourceNsxtPolicyQosProfileRead(d, m)
+}
+
+func resourceNsxtPolicyQosProfileDelete(d *schema.ResourceData, m interface{}) error {
+	id := d.Id()
+	if id == "" {
+		return fmt.Errorf("Error obtaining QosProfile ID")
+	}
+
+	var err error
+	connector := getPolicyConnector(m)
+	boolFalse := false
+	if isPolicyGlobalManager(m) {
+		client := gm_infra.NewDefaultQosProfilesClient(connector)
+		err = client.Delete(id, &boolFalse)
+	} else {
+		client := infra.NewDefaultQosProfilesClient(connector)
+		err = client.Delete(id, &boolFalse)
+	}
+
+	if err != nil {
+		return handleDeleteError("DhcpServer", id, err)
+	}
+
+	return nil
+}
---
+++ nsxt/resource_nsxt_policy_qos_profile_test.go
@@ -0,0 +1,204 @@
+/* Copyright © 2018 VMware, Inc. All Rights Reserved.
+   SPDX-License-Identifier: MPL-2.0 */
+
+package nsxt
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
+)
+
+func TestAccResourceNsxtPolicyQosProfile_basic(t *testing.T) {
+	name := getAccTestResourceName()
+	updatedName := getAccTestResourceName()
+	testResourceName := "nsxt_policy_qos_profile.test"
+	cos := "5"
+	updatedCos := "2"
+	peak := "700"
+	updatedPeak := "400"
+
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck:  func() { testAccPreCheck(t) },
+		Providers: testAccProviders,
+		CheckDestroy: func(state *terraform.State) error {
+			return testAccNSXQosSwitchingProfileCheckDestroy(state, updatedName)
+		},
+		Steps: []resource.TestStep{
+			{
+				Config: testAccNSXPolicyQosProfileBasicTemplate(name, cos, peak, "ingress"),
+				Check: resource.ComposeTestCheckFunc(
+					testAccNSXPolicyQosProfileExists(name, testResourceName),
+					resource.TestCheckResourceAttr(testResourceName, "display_name", name),
+					resource.TestCheckResourceAttr(testResourceName, "description", "test description"),
+					resource.TestCheckResourceAttr(testResourceName, "class_of_service", cos),
+					resource.TestCheckResourceAttr(testResourceName, "dscp_trusted", "true"),
+					resource.TestCheckResourceAttr(testResourceName, "dscp_priority", "53"),
+					resource.TestCheckResourceAttr(testResourceName, "ingress_rate_shaper.#", "1"),
+					resource.TestCheckResourceAttr(testResourceName, "ingress_rate_shaper.0.average_bw_mbps", "111"),
+					resource.TestCheckResourceAttr(testResourceName, "ingress_rate_shaper.0.burst_size", "222"),
+					resource.TestCheckResourceAttr(testResourceName, "ingress_rate_shaper.0.peak_bw_mbps", peak),
+					resource.TestCheckResourceAttr(testResourceName, "ingress_broadcast_rate_shaper.#", "1"),
+					resource.TestCheckResourceAttr(testResourceName, "ingress_broadcast_rate_shaper.0.average_bw_kbps", "111"),
+					resource.TestCheckResourceAttr(testResourceName, "ingress_broadcast_rate_shaper.0.burst_size", "222"),
+					resource.TestCheckResourceAttr(testResourceName, "ingress_broadcast_rate_shaper.0.peak_bw_kbps", peak),
+					resource.TestCheckResourceAttr(testResourceName, "tag.#", "1"),
+				),
+			},
+			{
+				Config: testAccNSXPolicyQosProfileBasicTemplate(updatedName, updatedCos, updatedPeak, "egress"),
+				Check: resource.ComposeTestCheckFunc(
+					testAccNSXPolicyQosProfileExists(updatedName, testResourceName),
+					resource.TestCheckResourceAttr(testResourceName, "display_name", updatedName),
+					resource.TestCheckResourceAttr(testResourceName, "description", "test description"),
+					resource.TestCheckResourceAttr(testResourceName, "class_of_service", updatedCos),
+					resource.TestCheckResourceAttr(testResourceName, "dscp_trusted", "true"),
+					resource.TestCheckResourceAttr(testResourceName, "dscp_priority", "53"),
+					resource.TestCheckResourceAttr(testResourceName, "egress_rate_shaper.#", "1"),
+					resource.TestCheckResourceAttr(testResourceName, "egress_rate_shaper.0.average_bw_mbps", "111"),
+					resource.TestCheckResourceAttr(testResourceName, "egress_rate_shaper.0.burst_size", "222"),
+					resource.TestCheckResourceAttr(testResourceName, "egress_rate_shaper.0.peak_bw_mbps", updatedPeak),
+					resource.TestCheckResourceAttr(testResourceName, "ingress_broadcast_rate_shaper.#", "1"),
+					resource.TestCheckResourceAttr(testResourceName, "ingress_broadcast_rate_shaper.0.average_bw_kbps", "111"),
+					resource.TestCheckResourceAttr(testResourceName, "ingress_broadcast_rate_shaper.0.burst_size", "222"),
+					resource.TestCheckResourceAttr(testResourceName, "ingress_broadcast_rate_shaper.0.peak_bw_kbps", updatedPeak),
+					resource.TestCheckResourceAttr(testResourceName, "tag.#", "1"),
+				),
+			},
+			{
+				Config: testAccNSXPolicyQosProfileEmptyTemplate(updatedName),
+				Check: resource.ComposeTestCheckFunc(
+					testAccNSXPolicyQosProfileExists(updatedName, testResourceName),
+					resource.TestCheckResourceAttr(testResourceName, "egress_rate_shaper.#", "0"),
+					resource.TestCheckResourceAttr(testResourceName, "ingress_broadcast_rate_shaper.#", "0"),
+					resource.TestCheckResourceAttr(testResourceName, "tag.#", "0"),
+				),
+			},
+		},
+	})
+}
+
+func TestAccResourceNsxtPolicyQosProfile_importBasic(t *testing.T) {
+	name := getAccTestResourceName()
+	testResourceName := "nsxt_policy_qos_profile.test"
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck:  func() { testAccPreCheck(t) },
+		Providers: testAccProviders,
+		CheckDestroy: func(state *terraform.State) error {
+			return testAccNSXPolicyQosProfileCheckDestroy(state, name)
+		},
+		Steps: []resource.TestStep{
+			{
+				Config: testAccNSXPolicyQosProfileCreateTemplateTrivial(name),
+			},
+			{
+				ResourceName:      testResourceName,
+				ImportState:       true,
+				ImportStateVerify: true,
+			},
+		},
+	})
+}
+
+func testAccNSXPolicyQosProfileExists(displayName string, resourceName string) resource.TestCheckFunc {
+	return func(state *terraform.State) error {
+		connector := getPolicyConnector(testAccProvider.Meta().(nsxtClients))
+
+		rs, ok := state.RootModule().Resources[resourceName]
+		if !ok {
+			return fmt.Errorf("Policy QosProfile resource %s not found in resources", resourceName)
+		}
+
+		resourceID := rs.Primary.ID
+		if resourceID == "" {
+			return fmt.Errorf("Policy QosProfile resource ID not set in resources")
+		}
+
+		exists, err := resourceNsxtPolicyQosProfileExists(resourceID, connector, testAccIsGlobalManager())
+		if err != nil {
+			return err
+		}
+		if !exists {
+			return fmt.Errorf("Error while retrieving policy QosProfile ID %s. Error: %v", resourceID, err)
+		}
+
+		return nil
+	}
+}
+
+func testAccNSXPolicyQosProfileCheckDestroy(state *terraform.State, displayName string) error {
+	connector := getPolicyConnector(testAccProvider.Meta().(nsxtClients))
+	for _, rs := range state.RootModule().Resources {
+
+		if rs.Type != "nsxt_policy_qos_profile" {
+			continue
+		}
+
+		resourceID := rs.Primary.Attributes["id"]
+		exists, err := resourceNsxtPolicyQosProfileExists(resourceID, connector, testAccIsGlobalManager())
+		if err == nil {
+			return err
+		}
+
+		if exists {
+			return fmt.Errorf("Policy QosProfile %s still exists", displayName)
+		}
+	}
+	return nil
+}
+
+func testAccNSXPolicyQosProfileBasicTemplate(name string, cos string, peak string, direction string) string {
+	return fmt.Sprintf(`
+resource "nsxt_policy_qos_profile" "test" {
+  display_name     = "%s"
+  description      = "test description"
+  class_of_service = %s
+  dscp_trusted     = true
+  dscp_priority    = 53
+
+  %s_rate_shaper {
+    average_bw_mbps = 111
+    burst_size      = 222
+    peak_bw_mbps    = "%s"
+  }
+
+  ingress_broadcast_rate_shaper {
+    average_bw_kbps = 111
+    burst_size      = 222
+    peak_bw_kbps    = "%s"
+  }
+
+  tag {
+    scope = "scope1"
+    tag   = "tag1"
+  }
+}
+`, name, cos, direction, peak, peak)
+}
+
+func testAccNSXPolicyQosProfileEmptyTemplate(name string) string {
+	return fmt.Sprintf(`
+resource "nsxt_policy_qos_profile" "test" {
+  display_name     = "%s"
+}
+`, name)
+}
+
+func testAccNSXPolicyQosProfileCreateTemplateTrivial(name string) string {
+	return fmt.Sprintf(`
+resource "nsxt_policy_qos_profile" "test" {
+  display_name = "%s"
+  description  = "test description"
+  dscp_trusted = false
+
+  egress_rate_shaper {
+    enabled         = false
+    peak_bw_mbps    = 800
+    burst_size      = 222
+    average_bw_mbps = 111
+  }
+}
+`, name)
+}
