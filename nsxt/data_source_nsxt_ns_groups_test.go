5cbe66085db46321d91b2be5c1a878c5dc1c238e
Anna Khmelnitsky <akhmelnitsky@vmware.com>
2021-09-01 01:11:10 +0000

    Introduces map data for ns groups and ns services
    Such data source builds a name to uuids map of the whole table.
    This map can be referenced in configuration to obtain object uuids at
    a cost of single roudtrip to NSX, which improves apply and refresh
    time at scale.
    
    Signed-off-by: Anna Khmelnitsky <akhmelnitsky@vmware.com>

---
+++ nsxt/data_source_nsxt_ns_groups_test.go
@@ -0,0 +1,58 @@
+/* Copyright © 2017 VMware, Inc. All Rights Reserved.
+   SPDX-License-Identifier: MPL-2.0 */
+
+package nsxt
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
+)
+
+func TestAccDataSourceNsxtNsGroups_basic(t *testing.T) {
+	groupName := getAccTestDataSourceName()
+	testResourceName := "data.nsxt_ns_groups.test"
+	// in order to verify correct functionality, we compare fetching by name with regular data source
+	// and fetching using the map data source with same name
+	// in order to verify map fetching with pair comparison, we use an extra resource
+	checkDataSourceName := "data.nsxt_ns_group.check"
+	checkResourceName := "nsxt_ns_group.check"
+
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck:  func() { testAccOnlyLocalManager(t); testAccTestMP(t); testAccPreCheck(t) },
+		Providers: testAccProviders,
+		CheckDestroy: func(state *terraform.State) error {
+			return testAccDataSourceNsxtNsGroupDeleteByName(groupName)
+		},
+		Steps: []resource.TestStep{
+			{
+				PreConfig: func() {
+					if err := testAccDataSourceNsxtNsGroupCreate(groupName); err != nil {
+						panic(err)
+					}
+				},
+				Config: testAccNSXNsGroupsReadTemplate(groupName),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttrSet(testResourceName, "id"),
+					resource.TestCheckResourceAttrPair(checkResourceName, "display_name", checkDataSourceName, "id"),
+				),
+			},
+		},
+	})
+}
+
+func testAccNSXNsGroupsReadTemplate(groupName string) string {
+	return fmt.Sprintf(`
+data "nsxt_ns_groups" "test" {
+}
+
+data "nsxt_ns_group" "check" {
+  display_name = "%s"
+}
+
+resource "nsxt_ns_group" "check" {
+  display_name = data.nsxt_ns_groups.test.items["%s"]
+}`, groupName, groupName)
+}
