3bf09db6ea4e2e80dffe1c245bda6c55f4284555
Anna Khmelnitsky <akhmelnitsky@vmware.com>
2023-04-06 20:26:29 +0000

    Add filtering options to vms data source
    VMs data source map can now be filtered by power state and
    operation system.
    
    Signed-off-by: Anna Khmelnitsky <akhmelnitsky@vmware.com>

--- nsxt/data_source_nsxt_policy_vms.go
+++ nsxt/data_source_nsxt_policy_vms.go
@@ -5,14 +5,28 @@ package nsxt
 
 import (
 	"fmt"
+	"strings"
 
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/validation"
+	"github.com/vmware/vsphere-automation-sdk-go/services/nsxt/model"
 )
 
 var valueTypeValues = []string{"bios_id", "external_id", "instance_id"}
 
+var stateMap = map[string]string{
+	"running":   model.VirtualMachine_POWER_STATE_VM_RUNNING,
+	"stopped":   model.VirtualMachine_POWER_STATE_VM_STOPPED,
+	"suspended": model.VirtualMachine_POWER_STATE_VM_SUSPENDED,
+	"unknown":   model.VirtualMachine_POWER_STATE_UNKNOWN,
+}
+
 func dataSourceNsxtPolicyVMs() *schema.Resource {
+	stateMapKeys := []string{}
+	for k := range stateMap {
+		stateMapKeys = append(stateMapKeys, k)
+	}
+
 	return &schema.Resource{
 		Read: dataSourceNsxtPolicyVMsRead,
 
@@ -24,6 +38,17 @@ func dataSourceNsxtPolicyVMs() *schema.Resource {
 				Optional:     true,
 				ValidateFunc: validation.StringInSlice(valueTypeValues, false),
 				Default:      "bios_id",
+			},
+			"state": {
+				Type:         schema.TypeString,
+				Description:  "Power state of the VM",
+				Optional:     true,
+				ValidateFunc: validation.StringInSlice(stateMapKeys, false),
+			},
+			"guest_os": {
+				Type:        schema.TypeString,
+				Description: "Operating system",
+				Optional:    true,
 			},
 			"items": {
 				Type:        schema.TypeMap,
@@ -41,6 +66,8 @@ func dataSourceNsxtPolicyVMsRead(d *schema.ResourceData, m interface{}) error {
 	connector := getPolicyConnector(m)
 
 	valueType := d.Get("value_type").(string)
+	state := d.Get("state").(string)
+	osPrefix := d.Get("guest_os").(string)
 	vmMap := make(map[string]interface{})
 
 	allVMs, err := listAllPolicyVirtualMachines(connector, m)
@@ -49,6 +76,20 @@ func dataSourceNsxtPolicyVMsRead(d *schema.ResourceData, m interface{}) error {
 	}
 
 	for _, vm := range allVMs {
+		if state != "" {
+			if vm.PowerState != nil && *vm.PowerState != stateMap[state] {
+				continue
+			}
+		}
+
+		if osPrefix != "" {
+			if vm.GuestInfo != nil && vm.GuestInfo.OsName != nil {
+				osName := strings.ToLower(*vm.GuestInfo.OsName)
+				if !strings.HasPrefix(osName, strings.ToLower(osPrefix)) {
+					continue
+				}
+			}
+		}
 		if vm.DisplayName == nil {
 			continue
 		}
