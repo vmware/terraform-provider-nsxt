50cb705ac03a2d656e3506d2e154b585b45bf190
Kobi Samoray <ksamoray@vmware.com>
2023-08-27 12:34:25 +0300

    Compute manager data source
    Signed-off-by: Kobi Samoray <ksamoray@vmware.com>

---
+++ nsxt/data_source_nsxt_compute_manager_test.go
@@ -0,0 +1,44 @@
+/* Copyright © 2023 VMware, Inc. All Rights Reserved.
+   SPDX-License-Identifier: MPL-2.0 */
+
+package nsxt
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+)
+
+func TestAccDataSourceNsxtComputeManager_basic(t *testing.T) {
+	ComputeManagerName := getComputeManagerName()
+	testResourceName := "data.nsxt_edge_cluster.test"
+
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck: func() {
+			testAccOnlyLocalManager(t)
+			testAccTestMP(t)
+			testAccPreCheck(t)
+			testAccEnvDefined(t, "NSXT_TEST_COMPUTE_MANAGER")
+		},
+		Providers: testAccProviders,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccNSXComputeManagerReadTemplate(ComputeManagerName),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr(testResourceName, "display_name", ComputeManagerName),
+					resource.TestCheckResourceAttrSet(testResourceName, "id"),
+					resource.TestCheckResourceAttrSet(testResourceName, "description"),
+					resource.TestCheckResourceAttrSet(testResourceName, "server"),
+				),
+			},
+		},
+	})
+}
+
+func testAccNSXComputeManagerReadTemplate(name string) string {
+	return fmt.Sprintf(`
+data "nsxt_compute_manager" "test" {
+  display_name = "%s"
+}`, name)
+}
---
+++ website/docs/d/compute_manager.html.markdown
@@ -0,0 +1,30 @@
+---
+subcategory: "Fabric"
+layout: "nsxt"
+page_title: "NSXT: compute_manager"
+description: A Compute Manager data source.
+---
+
+# nsxt_mac_pool
+
+This data source provides information about a Compute Manager configured on NSX.
+
+## Example Usage
+
+```hcl
+data "nsxt_compute_manager" "test_vcenter" {
+  display_name = "test-vcenter"
+}
+```
+
+## Argument Reference
+
+* `id` - (Optional) The ID of Compute Manager to retrieve
+* `display_name` - (Optional) The Display Name of the Compute Manager to retrieve.
+
+## Attributes Reference
+
+In addition to arguments listed above, the following attributes are exported:
+
+* `description` - The description of the MAC pool.
+* `server` - IP address or hostname of compute manager.
