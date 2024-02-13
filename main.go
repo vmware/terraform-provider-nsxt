e2f0964a173e0db317f37b5c8a3d06d84d937085
Kobi Samoray <kobi.samoray@broadcom.com>
2024-02-13 12:14:22 +0200

    Add debug capabilities to main.go
    Add some necessary code for execution of the provider with a debugger,
    e.g delve.
    
    Signed-off-by: Kobi Samoray <kobi.samoray@broadcom.com>

--- main.go
+++ main.go
@@ -4,15 +4,33 @@
 package main
 
 import (
+	"context"
+	"flag"
+	"log"
+
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/plugin"
 	"github.com/vmware/terraform-provider-nsxt/nsxt"
 )
 
 func main() {
-	plugin.Serve(&plugin.ServeOpts{
+	var debugMode bool
+	flag.BoolVar(&debugMode, "debug", false, "set to true to run the provider with support for debuggers like delve")
+	flag.Parse()
+
+	opts := &plugin.ServeOpts{
 		ProviderFunc: func() *schema.Provider {
 			return nsxt.Provider()
 		},
-	})
+	}
+
+	if debugMode {
+		err := plugin.Debug(context.Background(), "registry.terraform.io/vmware/nsxt", opts)
+		if err != nil {
+			log.Fatal(err.Error())
+		}
+		return
+	}
+
+	plugin.Serve(opts)
 }
