c3742098eb3a562bcd6e6af8a3b7ff3fd51c9627
Anna Khmelnitsky <akhmelnitsky@vmware.com>
2024-08-27 16:21:27 +0000

    Fix merge
    Signed-off-by: Anna Khmelnitsky <akhmelnitsky@vmware.com>

--- nsxt/provider.go
+++ nsxt/provider.go
@@ -510,9 +510,8 @@ func Provider() *schema.Provider {
 			"nsxt_policy_gateway_connection":                           resourceNsxtPolicyGatewayConnection(),
 			"nsxt_vpc":                                                 resourceNsxtVpc(),
 			"nsxt_vpc_nat_rule":                                        resourceNsxtPolicyVpcNatRule(),
-			"nsxt_vpc_ip_address_allocation":                           resourceNsxtVpcIpAddressAllocation(),
 			"nsxt_policy_transit_gateway_attachment":                   resourceNsxtPolicyTransitGatewayAttachment(),
-			"nsxt_vpc_ip_address_allocation":                           resourceNsxtVpcIPAddressAllocation(),
+			"nsxt_vpc_ip_address_allocation":                           resourceNsxtVpcIpAddressAllocation(),
 			"nsxt_vpc_subnet":                                          resourceNsxtVpcSubnet(),
 		},
 
