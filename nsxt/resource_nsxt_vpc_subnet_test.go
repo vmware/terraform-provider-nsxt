d818efbf431979a277c5bd7dbf625e7732a722a1
Anna Khmelnitsky <akhmelnitsky@vmware.com>
2024-09-17 01:52:52 +0000

    Expand VPC Subnet resource to meet the API
    Signed-off-by: Anna Khmelnitsky <akhmelnitsky@vmware.com>

--- nsxt/resource_nsxt_vpc_subnet_test.go
+++ nsxt/resource_nsxt_vpc_subnet_test.go
@@ -11,14 +11,18 @@ import (
 	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
 )
 
+// TODO - change lease_time to non-default value when platform bug is fixed
 var accTestVpcSubnetCreateAttributes = map[string]string{
 	"display_name":     getAccTestResourceName(),
 	"description":      "terraform created",
 	"access_mode":      "Isolated",
 	"enabled":          "true",
 	"enable_dhcp":      "true",
-	"ipv4_subnet_size": "64",
+	"ipv4_subnet_size": "32",
 	"ip_addresses":     "192.168.240.0/26",
+	"ntp_servers":      "192.168.4.14",
+	"lease_time":       "86400",
+	"excluded_ips":     "2.2.2.2",
 }
 
 var accTestVpcSubnetUpdateAttributes = map[string]string{
@@ -27,8 +31,11 @@ var accTestVpcSubnetUpdateAttributes = map[string]string{
 	"access_mode":      "Isolated",
 	"enabled":          "true",
 	"enable_dhcp":      "true",
-	"ipv4_subnet_size": "64",
+	"ipv4_subnet_size": "32",
 	"ip_addresses":     "192.168.240.0/26",
+	"ntp_servers":      "192.168.4.15",
+	"lease_time":       "86400",
+	"excluded_ips":     "2.2.2.3",
 }
 
 func TestAccResourceNsxtVpcSubnet_basic(t *testing.T) {
@@ -87,6 +94,86 @@ func TestAccResourceNsxtVpcSubnet_basic(t *testing.T) {
 					resource.TestCheckResourceAttrSet(testResourceName, "path"),
 					resource.TestCheckResourceAttrSet(testResourceName, "revision"),
 					resource.TestCheckResourceAttr(testResourceName, "tag.#", "0"),
+				),
+			},
+		},
+	})
+}
+
+func TestAccResourceNsxtVpcSubnet_subnetSize(t *testing.T) {
+	testResourceName := "nsxt_vpc_subnet.test"
+
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck: func() {
+			testAccPreCheck(t)
+			testAccOnlyVPC(t)
+		},
+		Providers: testAccProviders,
+		CheckDestroy: func(state *terraform.State) error {
+			return testAccNsxtVpcSubnetCheckDestroy(state, accTestVpcSubnetUpdateAttributes["display_name"])
+		},
+		Steps: []resource.TestStep{
+			{
+				Config: testAccNsxtVpcSubnetSizeTemplate(true),
+				Check: resource.ComposeTestCheckFunc(
+					testAccNsxtVpcSubnetExists(accTestVpcSubnetCreateAttributes["display_name"], testResourceName),
+					resource.TestCheckResourceAttr(testResourceName, "display_name", accTestVpcSubnetCreateAttributes["display_name"]),
+					resource.TestCheckResourceAttr(testResourceName, "description", accTestVpcSubnetCreateAttributes["description"]),
+					resource.TestCheckResourceAttr(testResourceName, "ipv4_subnet_size", accTestVpcSubnetCreateAttributes["ipv4_subnet_size"]),
+					resource.TestCheckResourceAttr(testResourceName, "ip_addresses.#", "1"),
+					resource.TestCheckResourceAttr(testResourceName, "dhcp_config.#", "1"),
+					resource.TestCheckResourceAttr(testResourceName, "dhcp_config.0.enable_dhcp", "true"),
+					resource.TestCheckResourceAttr(testResourceName, "dhcp_config.0.ntp_servers.#", "1"),
+					resource.TestCheckResourceAttr(testResourceName, "dhcp_config.0.ntp_servers.0", accTestVpcSubnetCreateAttributes["ntp_servers"]),
+					resource.TestCheckResourceAttr(testResourceName, "dhcp_config.0.lease_time", accTestVpcSubnetCreateAttributes["lease_time"]),
+					resource.TestCheckResourceAttr(testResourceName, "dhcp_config.0.excluded_ips.#", "1"),
+					resource.TestCheckResourceAttr(testResourceName, "dhcp_config.0.excluded_ips.0", accTestVpcSubnetCreateAttributes["excluded_ips"]),
+					resource.TestCheckResourceAttr(testResourceName, "dhcp_config.0.options.#", "1"),
+					resource.TestCheckResourceAttr(testResourceName, "dhcp_config.0.options.0.option121.#", "1"),
+					resource.TestCheckResourceAttr(testResourceName, "dhcp_config.0.options.0.other.#", "1"),
+
+					resource.TestCheckResourceAttrSet(testResourceName, "nsx_id"),
+					resource.TestCheckResourceAttrSet(testResourceName, "path"),
+					resource.TestCheckResourceAttrSet(testResourceName, "revision"),
+					resource.TestCheckResourceAttr(testResourceName, "tag.#", "0"),
+				),
+			},
+			{
+				Config: testAccNsxtVpcSubnetSizeTemplate(false),
+				Check: resource.ComposeTestCheckFunc(
+					testAccNsxtVpcSubnetExists(accTestVpcSubnetUpdateAttributes["display_name"], testResourceName),
+					resource.TestCheckResourceAttr(testResourceName, "display_name", accTestVpcSubnetUpdateAttributes["display_name"]),
+					resource.TestCheckResourceAttr(testResourceName, "description", accTestVpcSubnetUpdateAttributes["description"]),
+					resource.TestCheckResourceAttr(testResourceName, "ip_addresses.#", "1"),
+					resource.TestCheckResourceAttr(testResourceName, "ipv4_subnet_size", accTestVpcSubnetCreateAttributes["ipv4_subnet_size"]),
+					resource.TestCheckResourceAttr(testResourceName, "dhcp_config.#", "1"),
+					resource.TestCheckResourceAttr(testResourceName, "dhcp_config.0.enable_dhcp", "true"),
+					resource.TestCheckResourceAttr(testResourceName, "dhcp_config.0.ntp_servers.#", "1"),
+					resource.TestCheckResourceAttr(testResourceName, "dhcp_config.0.ntp_servers.0", accTestVpcSubnetUpdateAttributes["ntp_servers"]),
+					resource.TestCheckResourceAttr(testResourceName, "dhcp_config.0.lease_time", accTestVpcSubnetUpdateAttributes["lease_time"]),
+					resource.TestCheckResourceAttr(testResourceName, "dhcp_config.0.excluded_ips.#", "1"),
+					resource.TestCheckResourceAttr(testResourceName, "dhcp_config.0.excluded_ips.0", accTestVpcSubnetUpdateAttributes["excluded_ips"]),
+					resource.TestCheckResourceAttr(testResourceName, "dhcp_config.0.options.#", "1"),
+					resource.TestCheckResourceAttr(testResourceName, "dhcp_config.0.options.0.option121.#", "1"),
+					resource.TestCheckResourceAttr(testResourceName, "dhcp_config.0.options.0.other.#", "1"),
+
+					resource.TestCheckResourceAttrSet(testResourceName, "nsx_id"),
+					resource.TestCheckResourceAttrSet(testResourceName, "path"),
+					resource.TestCheckResourceAttrSet(testResourceName, "revision"),
+					resource.TestCheckResourceAttr(testResourceName, "tag.#", "0"),
+				),
+			},
+			{
+				Config: testAccNsxtVpcSubnetMinimalistic(),
+				Check: resource.ComposeTestCheckFunc(
+					testAccNsxtVpcSubnetExists(accTestVpcSubnetCreateAttributes["display_name"], testResourceName),
+					resource.TestCheckResourceAttr(testResourceName, "description", ""),
+					resource.TestCheckResourceAttrSet(testResourceName, "nsx_id"),
+					resource.TestCheckResourceAttrSet(testResourceName, "path"),
+					resource.TestCheckResourceAttrSet(testResourceName, "revision"),
+					resource.TestCheckResourceAttr(testResourceName, "tag.#", "0"),
+					resource.TestCheckResourceAttr(testResourceName, "dhcp_config.#", "1"),
+					resource.TestCheckResourceAttr(testResourceName, "dhcp_config.0.options.#", "0"),
 				),
 			},
 		},
@@ -168,7 +255,6 @@ func testAccNsxtVpcSubnetCheckDestroy(state *terraform.State, displayName string
 	return nil
 }
 
-// TODO - add subnet size option when NSX issue is fixed
 func testAccNsxtVpcSubnetTemplate(createFlow bool) string {
 	var attrMap map[string]string
 	if createFlow {
@@ -194,6 +280,45 @@ resource "nsxt_vpc_subnet" "test" {
     tag   = "tag1"
   }
 }`, testAccNsxtPolicyMultitenancyContext(), attrMap["display_name"], attrMap["description"], attrMap["ip_addresses"], attrMap["access_mode"], attrMap["enable_dhcp"])
+}
+
+func testAccNsxtVpcSubnetSizeTemplate(createFlow bool) string {
+	var attrMap map[string]string
+	if createFlow {
+		attrMap = accTestVpcSubnetCreateAttributes
+	} else {
+		attrMap = accTestVpcSubnetUpdateAttributes
+	}
+
+	return fmt.Sprintf(`
+resource "nsxt_vpc_subnet" "test" {
+%s
+  display_name = "%s"
+  description  = "%s"
+
+  ipv4_subnet_size = %s
+  access_mode      = "Public"
+
+  dhcp_config {
+    enable_dhcp  = true
+    ntp_servers  = ["%s"]
+    lease_time   = %s
+    excluded_ips = ["%s"]
+
+    options {
+      option121 {
+        static_route {
+          network  = "2.1.1.0/24"
+          next_hop = "2.3.1.3"
+        }
+      }
+      other {
+        code   = "119"
+        values = ["abc", "def"]
+      }
+    }
+  }
+}`, testAccNsxtPolicyMultitenancyContext(), attrMap["display_name"], attrMap["description"], attrMap["ipv4_subnet_size"], attrMap["ntp_servers"], attrMap["lease_time"], attrMap["excluded_ips"])
 }
 
 func testAccNsxtVpcSubnetMinimalistic() string {
