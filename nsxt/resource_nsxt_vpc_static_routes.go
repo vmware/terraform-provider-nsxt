bf56af84e1891ddff2fb2dc89c6a2963ed452b3c
Anna Khmelnitsky <akhmelnitsky@vmware.com>
2024-10-10 21:45:27 +0000

    Mark partial update when NSX update fails for vpc
    After merging VPC branch, this will be refactored to apply
    to all resources as part of handleUpdateError routine.
    Signed-off-by: Anna Khmelnitsky <akhmelnitsky@vmware.com>

--- nsxt/resource_nsxt_vpc_static_routes.go
+++ nsxt/resource_nsxt_vpc_static_routes.go
@@ -197,6 +197,9 @@ func resourceNsxtVpcStaticRoutesUpdate(d *schema.ResourceData, m interface{}) er
 	client := clientLayer.NewStaticRoutesClient(connector)
 	_, err := client.Update(parents[0], parents[1], parents[2], id, obj)
 	if err != nil {
+		// Trigger partial update to avoid terraform updating state based on failed intent
+		// TODO - move this into handleUpdateError
+		d.Partial(true)
 		return handleUpdateError("StaticRoutes", id, err)
 	}
 
