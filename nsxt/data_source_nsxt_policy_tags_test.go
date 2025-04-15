88c8e64ec930eac44dec77d6590c37f3d4760872
Arun Tony <aruntony005@gmail.com>
2025-04-15 18:07:43 +0530

    Adding version check for tags data source acceptance tests
    Signed-off-by: Arun Tony <aruntony005@gmail.com>

--- nsxt/data_source_nsxt_policy_tags_test.go
+++ nsxt/data_source_nsxt_policy_tags_test.go
@@ -21,6 +21,7 @@ func TestAccDataSourceNsxtPolicyTags_basic(t *testing.T) {
 		PreCheck: func() {
 			testAccPreCheck(t)
 			testAccOnlyLocalManager(t)
+			testAccNSXVersion(t, "4.2.0")
 		},
 		Providers: testAccProviders,
 		Steps: []resource.TestStep{
