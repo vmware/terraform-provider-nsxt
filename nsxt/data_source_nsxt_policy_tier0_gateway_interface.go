89ac7922291541c21204b6db7bc217c115a094e1
Arun Tony <aruntony005@gmail.com>
2025-05-04 13:21:21 +0530

    Refactoring
    Signed-off-by: Arun Tony <aruntony005@gmail.com>

--- nsxt/data_source_nsxt_policy_tier0_gateway_interface.go
+++ nsxt/data_source_nsxt_policy_tier0_gateway_interface.go
@@ -46,8 +46,6 @@ func dataSourceNsxtPolicyTier0GatewayInterface() *schema.Resource {
 func dataSourceNsxtPolicyTier0GatewayInterfaceRead(d *schema.ResourceData, m interface{}) error {
 	connector := getPolicyConnector(m)
 	converter := bindings.NewTypeConverter()
-	// interfaceName := d.Get("display_name").(string)
-	// interfaceId := d.Get("id").(string)
 	t0Gw := d.Get("t0_gateway_path").(string)
 	query := make(map[string]string)
 	query["parent_path"] = t0Gw + "/locale-services/*"
@@ -55,16 +53,12 @@ func dataSourceNsxtPolicyTier0GatewayInterfaceRead(d *schema.ResourceData, m int
 	if err != nil {
 		return err
 	}
-	// return fmt.Errorf("debugggggg : %v", obj)
 	dataValue, errors := converter.ConvertToGolang(obj, model.Tier0InterfaceBindingType())
 	if len(errors) > 0 {
 		return errors[0]
 	}
 
 	currGwInterface := dataValue.(model.Tier0Interface)
-	// isT0, _, _, _ := parseGatewayInterfacePolicyPath(*currGwInterface.Path)
-
-	// if isT0 {
 	if currGwInterface.Path != nil {
 		err := d.Set("path", *currGwInterface.Path)
 		if err != nil {
@@ -92,94 +86,5 @@ func dataSourceNsxtPolicyTier0GatewayInterfaceRead(d *schema.ResourceData, m int
 		}
 	}
 	d.SetId(newUUID())
-
-	// }
-	// return fmt.Errorf("debugggggg : %v", *currGwInterface.DisplayName)
 	return nil
 }
-
-// func dataSourceNsxtPolicyTier0GatewayInterfaceRead(d *schema.ResourceData, m interface{}) error {
-// 	connector := getPolicyConnector(m)
-// 	converter := bindings.NewTypeConverter()
-// 	interfaceName := d.Get("display_name").(string)
-// 	interfaceId := d.Get("id").(string)
-// 	t0Gw := d.Get("t0_gateway_path").(string)
-
-// 	// Get the T0 gateway ID as in some case the gw name and the id are different
-// 	t0GwObj := GenericResourceData{
-// 		DisplayName: t0Gw,
-// 		Id:          "",
-// 	}
-// 	gwObjList, err := t0GwObj.policyGenericDataSourceResourceRead(connector, getSessionContext(d, m), "Tier0", nil)
-// 	if err != nil {
-// 		return err
-// 	}
-// 	t0GwList := []model.Tier0{}
-// 	for _, obj := range gwObjList {
-// 		dataValue, errors := converter.ConvertToGolang(obj, model.Tier0BindingType())
-// 		if len(errors) > 0 {
-// 			return errors[0]
-// 		}
-// 		curGw := dataValue.(model.Tier0)
-// 		if *curGw.DisplayName == t0Gw {
-// 			t0GwList = append(t0GwList, dataValue.(model.Tier0))
-// 		}
-// 	}
-
-// 	// Get the interface
-// 	interfaceObj := GenericResourceData{
-// 		DisplayName: interfaceName,
-// 		Id:          interfaceId,
-// 	}
-// 	interfaceObjList, err := interfaceObj.policyGenericDataSourceResourceRead(connector, getSessionContext(d, m), "Tier0Interface", nil)
-// 	if err != nil {
-// 		return err
-// 	}
-
-// 	isOp := false
-// 	for _, obj := range interfaceObjList {
-// 		dataValue, errors := converter.ConvertToGolang(obj, model.Tier0InterfaceBindingType())
-// 		if len(errors) > 0 {
-// 			return errors[0]
-// 		}
-// 		currInt := dataValue.(model.Tier0Interface)
-// 		isT0, gwID, _, _ := parseGatewayInterfacePolicyPath(*currInt.Path)
-
-// 		if isT0 && gwID == *t0GwList[0].Id {
-// 			isOp = true
-// 			if currInt.Path != nil {
-// 				err := d.Set("path", *currInt.Path)
-// 				if err != nil {
-// 					return fmt.Errorf("Error while setting interface path : %v", err)
-// 				}
-// 			}
-// 			if isPolicyGlobalManager(m) {
-// 				d.Set("edge_cluster_path", "")
-// 			} else if currInt.EdgePath != nil {
-// 				err = d.Set("edge_cluster_path", *currInt.EdgePath)
-// 				if err != nil {
-// 					return fmt.Errorf("Error while setting the interface edge cluster path : %v", err)
-// 				}
-// 			}
-// 			if currInt.Description != nil {
-// 				err = d.Set("description", *currInt.Description)
-// 				if err != nil {
-// 					return fmt.Errorf("Error while setting the interface description : %v", err)
-// 				}
-// 			}
-// 			if currInt.SegmentPath != nil {
-// 				err = d.Set("segment_path", *currInt.SegmentPath)
-// 				if err != nil {
-// 					return fmt.Errorf("Error while setting the segment connected to the interface : %v", err)
-// 				}
-// 			}
-// 			d.SetId(newUUID())
-// 			break
-// 		}
-// 	}
-// 	if !isOp {
-// 		return fmt.Errorf("The T0 gateway %s doesn't have a linked interface with name %s.", t0Gw, interfaceName)
-// 	}
-// 	return nil
-
-// }
