5c648aa3f54e4f0dbe3725816a64641edbdef453
Pooja V <pooja.venkatesh@broadcom.com>
2025-06-04 22:19:45 +0530

    Add Static Route support to Transit Gateway resource

---
+++ docs/resources/policy_transit_gateway_static_routes.md
@@ -0,0 +1,68 @@
+---
+subcategory: "FIXME"
+page_title: "NSXT: nsxt_policy_transit_gateway_static_routes"
+description: A resource to configure a StaticRoutes.
+---
+
+# nsxt_policy_transit_gateway_static_routes
+
+This resource provides a method for the management of a transit gateway StaticRoutes.
+
+This resource is applicable to NSX Global Manager, NSX Policy Manager and VMC.
+
+## Example Usage
+
+```hcl
+data "nsxt_policy_project" "proj" {
+  display_name = "demoproj"
+}
+
+data "nsxt_policy_transit_gateway" "tgw1" {
+  context {
+    project_id = data.nsxt_policy_project.proj.id
+  }
+  display_name = "TGW1"
+}
+
+resource "nsxt_policy_transit_gateway_static_routes" "test" {
+  display_name           = "test"
+  description            = "Terraform provisioned StaticRoutes for transit gateway"
+  parent_path            = data.nsxt_policy_transit_gateway.tgw1.path
+  enabled_on_secondary   = false
+}
+
+```
+
+## Argument Reference
+
+The following arguments are supported:
+
+* `display_name` - (Required) Display name of the resource.
+* `description` - (Optional) Description of the resource.
+* `tag` - (Optional) A list of scope + tag pairs to associate with this resource.
+* `nsx_id` - (Optional) The NSX ID of this resource. If set, this ID will be used to create the resource.
+* `parent_path` - (Required) Path of parent object
+* `network` - (Optional) Specify network address in CIDR format.
+* `next_hop` - (Optional) Specify next hop routes for network.
+* `enabled_on_secondary` - (Optional) Flag to plumb route on secondary site.
+
+
+## Attributes Reference
+
+In addition to arguments listed above, the following attributes are exported:
+
+* `id` - ID of the resource.
+* `revision` - Indicates current revision number of the object as seen by NSX-T API server. This attribute can be useful for debugging.
+* `path` - The NSX path of the policy resource.
+
+## Importing
+
+An existing object can be [imported][docs-import] into this resource, via the following command:
+
+[docs-import]: https://www.terraform.io/cli/import
+
+```
+terraform import nsxt_policy_transit_gateway_static_routes.test PATH
+```
+
+The above command imports StaticRoutes named `test` with the NSX path `PATH`.
---
+++ nsxt/resource_nsxt_policy_transit_gateway_static_routes_test.go
@@ -0,0 +1,215 @@
+// © Broadcom. All Rights Reserved.
+// The term "Broadcom" refers to Broadcom Inc. and/or its subsidiaries.
+// SPDX-License-Identifier: MPL-2.0
+
+package nsxt
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
+)
+
+var accTestPolicyTransitGatewayStaticRoutesCreateAttributes = map[string]string{
+	"display_name":         getAccTestResourceName(),
+	"description":          "terraform created",
+	"enabled_on_secondary": "true",
+	"network":              "2.2.2.0/24",
+	"ip_address":           "3.1.1.1",
+	"admin_distance":       "2",
+}
+
+var accTestPolicyTransitGatewayStaticRoutesUpdateAttributes = map[string]string{
+	"display_name":         getAccTestResourceName(),
+	"description":          "terraform updated",
+	"enabled_on_secondary": "false",
+	"network":              "3.3.3.0/24",
+	"ip_address":           "4.1.1.1",
+	"admin_distance":       "5",
+}
+
+func TestAccResourceNsxtPolicyTransitGatewayStaticRoutes_basic(t *testing.T) {
+	testResourceName := "nsxt_policy_transit_gateway_static_routes.test"
+
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck:  func() { testAccPreCheck(t) },
+		Providers: testAccProviders,
+		CheckDestroy: func(state *terraform.State) error {
+			return testAccNsxtPolicyTransitGatewayStaticRoutesCheckDestroy(state, accTestPolicyTransitGatewayStaticRoutesUpdateAttributes["display_name"])
+		},
+		Steps: []resource.TestStep{
+			{
+				Config: testAccNsxtPolicyTransitGatewayStaticRoutesTemplate(true),
+				Check: resource.ComposeTestCheckFunc(
+					testAccNsxtPolicyTransitGatewayStaticRoutesExists(accTestPolicyTransitGatewayStaticRoutesCreateAttributes["display_name"], testResourceName),
+					resource.TestCheckResourceAttr(testResourceName, "display_name", accTestPolicyTransitGatewayStaticRoutesCreateAttributes["display_name"]),
+					resource.TestCheckResourceAttr(testResourceName, "description", accTestPolicyTransitGatewayStaticRoutesCreateAttributes["description"]),
+					resource.TestCheckResourceAttr(testResourceName, "network", accTestPolicyTransitGatewayStaticRoutesCreateAttributes["network"]),
+					resource.TestCheckResourceAttr(testResourceName, "next_hop.#", "1"),
+					resource.TestCheckResourceAttr(testResourceName, "enabled_on_secondary", accTestPolicyTransitGatewayStaticRoutesCreateAttributes["enabled_on_secondary"]),
+					resource.TestCheckResourceAttr(testResourceName, "next_hop.0.ip_address", accTestPolicyTransitGatewayStaticRoutesCreateAttributes["ip_address"]),
+					resource.TestCheckResourceAttr(testResourceName, "next_hop.0.admin_distance", accTestPolicyTransitGatewayStaticRoutesCreateAttributes["admin_distance"]),
+
+					resource.TestCheckResourceAttrSet(testResourceName, "nsx_id"),
+					resource.TestCheckResourceAttrSet(testResourceName, "path"),
+					resource.TestCheckResourceAttrSet(testResourceName, "revision"),
+					resource.TestCheckResourceAttr(testResourceName, "tag.#", "1"),
+				),
+			},
+			{
+				Config: testAccNsxtPolicyTransitGatewayStaticRoutesTemplate(false),
+				Check: resource.ComposeTestCheckFunc(
+					testAccNsxtPolicyTransitGatewayStaticRoutesExists(accTestPolicyTransitGatewayStaticRoutesUpdateAttributes["display_name"], testResourceName),
+					resource.TestCheckResourceAttr(testResourceName, "display_name", accTestPolicyTransitGatewayStaticRoutesUpdateAttributes["display_name"]),
+					resource.TestCheckResourceAttr(testResourceName, "description", accTestPolicyTransitGatewayStaticRoutesUpdateAttributes["description"]),
+					resource.TestCheckResourceAttr(testResourceName, "network", accTestPolicyTransitGatewayStaticRoutesUpdateAttributes["network"]),
+					resource.TestCheckResourceAttr(testResourceName, "next_hop.#", "1"),
+					resource.TestCheckResourceAttr(testResourceName, "enabled_on_secondary", accTestPolicyTransitGatewayStaticRoutesUpdateAttributes["enabled_on_secondary"]),
+					resource.TestCheckResourceAttr(testResourceName, "next_hop.0.ip_address", accTestPolicyTransitGatewayStaticRoutesUpdateAttributes["ip_address"]),
+					resource.TestCheckResourceAttr(testResourceName, "next_hop.0.admin_distance", accTestPolicyTransitGatewayStaticRoutesUpdateAttributes["admin_distance"]),
+
+					resource.TestCheckResourceAttrSet(testResourceName, "nsx_id"),
+					resource.TestCheckResourceAttrSet(testResourceName, "path"),
+					resource.TestCheckResourceAttrSet(testResourceName, "revision"),
+					resource.TestCheckResourceAttr(testResourceName, "tag.#", "1"),
+				),
+			},
+			{
+				Config: testAccNsxtPolicyTransitGatewayStaticRoutesMinimalistic(),
+				Check: resource.ComposeTestCheckFunc(
+					testAccNsxtPolicyTransitGatewayStaticRoutesExists(accTestPolicyTransitGatewayStaticRoutesCreateAttributes["display_name"], testResourceName),
+					resource.TestCheckResourceAttr(testResourceName, "description", ""),
+					resource.TestCheckResourceAttrSet(testResourceName, "nsx_id"),
+					resource.TestCheckResourceAttrSet(testResourceName, "path"),
+					resource.TestCheckResourceAttrSet(testResourceName, "revision"),
+					resource.TestCheckResourceAttr(testResourceName, "tag.#", "0"),
+				),
+			},
+		},
+	})
+}
+
+func TestAccResourceNsxtPolicyTransitGatewayStaticRoutes_importBasic(t *testing.T) {
+	name := getAccTestResourceName()
+	testResourceName := "nsxt_policy_transit_gateway_static_routes.test"
+
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck:  func() { testAccPreCheck(t) },
+		Providers: testAccProviders,
+		CheckDestroy: func(state *terraform.State) error {
+			return testAccNsxtPolicyTransitGatewayStaticRoutesCheckDestroy(state, name)
+		},
+		Steps: []resource.TestStep{
+			{
+				Config: testAccNsxtPolicyTransitGatewayStaticRoutesMinimalistic(),
+			},
+			{
+				ResourceName:      testResourceName,
+				ImportState:       true,
+				ImportStateVerify: true,
+			},
+		},
+	})
+}
+
+func testAccNsxtPolicyTransitGatewayStaticRoutesExists(displayName string, resourceName string) resource.TestCheckFunc {
+	return func(state *terraform.State) error {
+
+		connector := getPolicyConnector(testAccProvider.Meta().(nsxtClients))
+
+		rs, ok := state.RootModule().Resources[resourceName]
+		if !ok {
+			return fmt.Errorf("Policy TransitGatewayStaticRoutes resource %s not found in resources", resourceName)
+		}
+
+		resourceID := rs.Primary.ID
+		if resourceID == "" {
+			return fmt.Errorf("Policy ransitGatewayStaticRoutes resource ID not set in resources")
+		}
+		parentPath := rs.Primary.Attributes["parentPath"]
+		exists, err := resourceNsxtPolicyTransitGatewayStaticRoutesExists(testAccGetSessionContext(), parentPath, resourceID, connector)
+		if err != nil {
+			return err
+		}
+		if !exists {
+			return fmt.Errorf("Policy ransitGatewayStaticRoutes %s does not exist", resourceID)
+		}
+
+		return nil
+	}
+}
+
+func testAccNsxtPolicyTransitGatewayStaticRoutesCheckDestroy(state *terraform.State, displayName string) error {
+	connector := getPolicyConnector(testAccProvider.Meta().(nsxtClients))
+	for _, rs := range state.RootModule().Resources {
+
+		if rs.Type != "nsxt_policy_transit_gateway_static_routes" {
+			continue
+		}
+
+		resourceID := rs.Primary.Attributes["id"]
+		parentPath := rs.Primary.Attributes["parentPath"]
+		exists, err := resourceNsxtPolicyTransitGatewayStaticRoutesExists(testAccGetSessionContext(), parentPath, resourceID, connector)
+		if err == nil {
+			return err
+		}
+
+		if exists {
+			return fmt.Errorf("Policy ransitGatewayStaticRoutes %s still exists", displayName)
+		}
+	}
+	return nil
+}
+
+func testAccNsxtPolicyTransitGatewayStaticRoutesTemplate(createFlow bool) string {
+	var attrMap map[string]string
+	if createFlow {
+		attrMap = accTestPolicyTransitGatewayStaticRoutesCreateAttributes
+	} else {
+		attrMap = accTestPolicyTransitGatewayStaticRoutesUpdateAttributes
+	}
+	return fmt.Sprintf(`
+data "nsxt_policy_edge_cluster" "test" {
+  display_name = "testEdgeCluster"
+}
+
+resource "nsxt_policy_tier0_gateway" "test" {
+  display_name      = "testt0gw"
+  edge_cluster_path = data.nsxt_policy_edge_cluster.test.path
+}
+
+resource "nsxt_policy_project" "test" {
+  display_name             = "testProject"
+  tier0_gateway_paths      = [nsxt_policy_tier0_gateway.test.path]
+  site_info {
+    edge_cluster_paths = [data.nsxt_policy_edge_cluster.test.path]
+  }
+}
+
+data "nsxt_policy_transit_gateway" "test" {
+  context {
+    project_id = nsxt_policy_project.test.id
+  }
+  id = "default"
+}
+resource "nsxt_policy_transit_gateway_static_routes" "test" {
+  display_name = "%s"
+  description  = "%s"
+  parent_path = "data.nsxt_policy_transit_gateway.test.path"
+
+  tag {
+    scope = "scope1"
+    tag   = "tag1"
+  }
+}`, attrMap["display_name"], attrMap["description"])
+}
+
+func testAccNsxtPolicyTransitGatewayStaticRoutesMinimalistic() string {
+	return fmt.Sprintf(`
+resource "nsxt_policy_transit_gateway_static_routes" "test" {
+  display_name = "%s"
+
+}`, accTestPolicyTransitGatewayStaticRoutesUpdateAttributes["display_name"])
+}
