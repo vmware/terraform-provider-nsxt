dc7f148cec68ee2015e2f2802aa98736ddef5031
Kobi Samoray <kobi.samoray@broadcom.com>
2025-08-03 10:15:47 +0300

    Add a policy_share datasource
    To complete sharing functionality, add a datasource for policy_share.
    
    Signed-off-by: Kobi Samoray <kobi.samoray@broadcom.com>

---
+++ nsxt/data_source_nsxt_policy_share.go
@@ -0,0 +1,35 @@
+// © Broadcom. All Rights Reserved.
+// The term "Broadcom" refers to Broadcom Inc. and/or its subsidiaries.
+// SPDX-License-Identifier: MPL-2.0
+
+package nsxt
+
+import (
+	"fmt"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+)
+
+func dataSourceNsxtPolicyShare() *schema.Resource {
+	return &schema.Resource{
+		Read: dataSourceNsxtPolicyShareRead,
+
+		Schema: map[string]*schema.Schema{
+			"id":           getDataSourceIDSchema(),
+			"path":         getPathSchema(),
+			"display_name": getDisplayNameSchema(),
+			"description":  getDescriptionSchema(),
+		},
+	}
+}
+
+func dataSourceNsxtPolicyShareRead(d *schema.ResourceData, m interface{}) error {
+	connector := getPolicyConnector(m)
+
+	_, err := policyDataSourceResourceReadWithValidation(d, connector, getSessionContext(d, m), "Share", nil, false)
+	if err == nil {
+		return nil
+	}
+
+	return fmt.Errorf("Share with name '%s' was not found", d.Get("display_name").(string))
+}
