74cee3c9fac03860254c0c6c6f7b5b2e0438032c
Arun Tony <aruntony005@gmail.com>
2025-08-26 13:05:16 +0530

    Fixing lint issues
    Signed-off-by: Arun Tony <aruntony005@gmail.com>

--- nsxt/resource_nsxt_policy_gateway_policy_rule.go
+++ nsxt/resource_nsxt_policy_gateway_policy_rule.go
@@ -19,15 +19,14 @@ func resourceNsxtPolicyGatewayRulePolicy() *schema.Resource {
 		Importer: &schema.ResourceImporter{
 			State: nsxtDomainResourceImporter,
 		},
-		
+
 		Schema: getSecurityPolicyAndGatewayRuleSchema(false, false, false, true),
 	}
 }
 
-
 func resourceNsxtPolicyGatewayPolicyRuleCreate(d *schema.ResourceData, m interface{}) error {
 	connector := getPolicyConnector(m)
-	
+
 	ruleName := d.Get("display_name").(string)
 	policyPath := d.Get("policy_path").(string)
 	projectID := getProjectIDFromResourcePath(policyPath)
@@ -54,7 +53,7 @@ func resourceNsxtPolicyGatewayPolicyRuleCreate(d *schema.ResourceData, m interfa
 	if err != nil {
 		return handleCreateError("GatewayPolicyRule", fmt.Sprintf("%s/%s", policyPath, ruleName), err)
 	}
-	
+
 	d.SetId(ruleName)
 
 	return resourceNsxtPolicyGatewayPolicyRuleRead(d, m)
@@ -72,19 +71,19 @@ func resourceNsxtPolicyGatewayPolicyRuleExists(sessionContext utl.SessionContext
 	if client == nil {
 		return false, policyResourceNotSupportedError()
 	}
-	
+
 	domain := getDomainFromResourcePath(policyPath)
 	policyID := getPolicyIDFromPath(policyPath)
 	_, err := client.Get(domain, policyID, id)
-	
+
 	if err == nil {
 		return true, nil
 	}
-	
+
 	if isNotFoundError(err) {
 		return false, nil
 	}
-	
+
 	return false, logAPIError("Error retrieving Security Policy Rule", err)
 }
 
@@ -94,13 +93,12 @@ func resourceNsxtPolicyGatewayPolicyRuleRead(d *schema.ResourceData, m interface
 	if id == "" {
 		return fmt.Errorf("Error obtaining Gateway Policy Rule ID")
 	}
-	
+
 	policyPath := d.Get("policy_path").(string)
 	projectID := getProjectIDFromResourcePath(policyPath)
 	domain := getDomainFromResourcePath(policyPath)
 	policyID := getPolicyIDFromPath(policyPath)
-	
-	
+
 	if err := setSecurityOrGatewayPolicyRuleContext(d, projectID); err != nil {
 		return handleReadError(d, "GatewayPolicyRule", fmt.Sprintf("%s/%s", policyPath, id), err)
 	}
@@ -113,19 +111,19 @@ func resourceNsxtPolicyGatewayPolicyRuleRead(d *schema.ResourceData, m interface
 	if err != nil {
 		return handleReadError(d, "GatewayPolicyRule", fmt.Sprintf("%s/%s", policyPath, id), err)
 	}
-	
+
 	securityAndGatewayPolicyRuleModelToSchema(d, rule)
 	return nil
 }
 
 func resourceNsxtPolicyGatewayPolicyRuleUpdate(d *schema.ResourceData, m interface{}) error {
 	connector := getPolicyConnector(m)
-	
+
 	id := d.Id()
 	if id == "" {
 		return fmt.Errorf("Error obtaining Gateway Policy Rule ID")
 	}
-	
+
 	policyPath := d.Get("policy_path").(string)
 	log.Printf("[INFO] Updating Gateway Policy Rule with ID %s under policy %s", id, policyPath)
 	domain := getDomainFromResourcePath(policyPath)
@@ -141,7 +139,7 @@ func resourceNsxtPolicyGatewayPolicyRuleUpdate(d *schema.ResourceData, m interfa
 	if err != nil {
 		return handleUpdateError("GatewayPolicyRule", fmt.Sprintf("%s/%s", policyPath, id), err)
 	}
-	
+
 	return resourceNsxtPolicyGatewayPolicyRuleRead(d, m)
 }
 
@@ -150,18 +148,17 @@ func resourceNsxtPolicyGatewayPolicyRuleDelete(d *schema.ResourceData, m interfa
 	if id == "" {
 		return fmt.Errorf("Error obtaining Gateway Policy Rule ID")
 	}
-	
+
 	connector := getPolicyConnector(m)
-	
+
 	policyPath := d.Get("policy_path").(string)
 	log.Printf("[INFO] Deleting Gateway Policy Rule with ID %s under policy %s", id, policyPath)
 	domain := getDomainFromResourcePath(policyPath)
 	policyID := getPolicyIDFromPath(policyPath)
-	
+
 	client := gatewaypolicies.NewRulesClient(getSessionContext(d, m), connector)
 	if client == nil {
 		return policyResourceNotSupportedError()
 	}
 	return client.Delete(domain, policyID, id)
 }
-
