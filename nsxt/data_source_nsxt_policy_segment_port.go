7183f9836404b3016a11adf2bdb51f43d81044e0
Arun Tony <aruntony005@gmail.com>
2025-10-15 02:34:59 +0530

    Added nsxt_policy_segment_port resource with the CRUD functionalities.
    Updated the nsxt_policy_vm data source to return vif attachment IDs of the VMs.
    
    Added nsxt_policy_segment_port data source to get the segment ports with the display name or the vif id from the VMs data source.
    
    Updated the T1 segment port client code.
    
    Fixes: #1814
    Signed-off-by: Arun Tony <aruntony005@gmail.com>

---
+++ nsxt/data_source_nsxt_policy_segment_port.go
@@ -0,0 +1,83 @@
+package nsxt
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+	"github.com/vmware/vsphere-automation-sdk-go/runtime/bindings"
+	"github.com/vmware/vsphere-automation-sdk-go/runtime/data"
+	"github.com/vmware/vsphere-automation-sdk-go/services/nsxt/model"
+)
+
+func dataSourceNsxtPolicySegmentPort() *schema.Resource {
+	return &schema.Resource{
+		Read: dataSourceNsxtPolicySegmentPortRead,
+
+		Schema: map[string]*schema.Schema{
+			"id":           getDataSourceIDSchema(),
+			"display_name": getDataSourceDisplayNameSchema(),
+			"description":  getDataSourceDescriptionSchema(),
+			"path":         getPathSchema(),
+			"context":      getContextSchema(false, false, false),
+			"vif_id": {
+				Type:        schema.TypeString,
+				Description: "Segment Port attachment id",
+				Optional:    true,
+			},
+			"segment_path": {
+				Type:        schema.TypeString,
+				Description: "Segment path",
+				Optional:    true,
+			},
+		},
+	}
+}
+
+func dataSourceNsxtPolicySegmentPortRead(d *schema.ResourceData, m interface{}) error {
+	connector := getPolicyConnector(m)
+	converter := bindings.NewTypeConverter()
+	var segmentPortList []*data.StructValue
+	var err error
+	if vifId, exists := d.GetOkExists("vif_id"); exists {
+		addQuery := fmt.Sprintf("attachment.id:%s", escapeSpecialCharacters(vifId.(string)))
+		segmentPortList, err = listPolicyResources(connector, getSessionContext(d, m), "SegmentPort", &addQuery)
+		if err != nil {
+			return fmt.Errorf("Error getting the Segment Port list : %v", err)
+		}
+		if len(segmentPortList) == 0 {
+			return fmt.Errorf("Segment Port with VIF attachment id %s not found", vifId)
+		}
+	} else if portName, exists := d.GetOkExists("display_name"); exists {
+		addQuery := fmt.Sprintf("display_name:%s", escapeSpecialCharacters(portName.(string)))
+		segmentPortList, err = listPolicyResources(connector, getSessionContext(d, m), "SegmentPort", &addQuery)
+		if err != nil {
+			return fmt.Errorf("Error getting the Segment Port list : %v", err)
+		}
+		if len(segmentPortList) == 0 {
+			return fmt.Errorf("Segment Port with display_name %s not found", portName)
+		}
+	} else {
+		return fmt.Errorf("Either vif_id or display_name should be set")
+	}
+
+	dataValue, errors := converter.ConvertToGolang(segmentPortList[0], model.SegmentPortBindingType())
+	if len(errors) > 0 {
+		return errors[0]
+	}
+	segmentPort := dataValue.(model.SegmentPort)
+	d.SetId(*segmentPort.Id)
+	d.Set("display_name", segmentPort.DisplayName)
+	d.Set("description", segmentPort.Description)
+	d.Set("path", segmentPort.Path)
+	d.Set("segment_path", getSegmentPathFromPortPath(*segmentPort.Path))
+
+	return nil
+}
+
+func getSegmentPathFromPortPath(portPath string) string {
+	parts := strings.Split(portPath, "/")
+	parts = parts[:len(parts)-2]
+
+	return strings.Join(parts, "/")
+}
---
+++ nsxt/resource_nsxt_policy_segment_port.go
@@ -0,0 +1,204 @@
+package nsxt
+
+import (
+	"fmt"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/validation"
+)
+
+func resourceNsxtPolicySegmentPort() *schema.Resource {
+	return &schema.Resource{
+		Create:   resourceNsxtPolicySegmentPortCreate,
+		Read:     resourceNsxtPolicySegmentPortRead,
+		Update:   resourceNsxtPolicySegmentPortUpdate,
+		Delete:   resourceNsxtPolicySegmentPortDelete,
+		Importer: &schema.ResourceImporter{}, // TODO: Add importer
+		Schema: map[string]*schema.Schema{
+			"nsx_id":       getNsxIDSchema(),
+			"path":         getPathSchema(),
+			"display_name": getDisplayNameSchema(),
+			"description":  getDescriptionSchema(),
+			"revision":     getRevisionSchema(),
+			"tag":          getTagsSchema(),
+			"segment_path": {
+				Type:        schema.TypeString,
+				Description: "Path of the segment",
+				Optional:    true,
+			},
+			"attachment": {
+				Type:        schema.TypeList,
+				Description: "VIF attachment",
+				Optional:    true,
+				Elem:        getPolicySegmentPortSchema(),
+				MaxItems:    1,
+			},
+			"vif_id": {
+				Type:        schema.TypeString,
+				Description: "Segment Port attachment id",
+				Optional:    true,
+			},
+			"discovery_profile": {
+				Type:        schema.TypeList,
+				Description: "IP and MAC discovery profiles for this segment",
+				Elem:        getPolicySegmentDiscoveryProfilesSchema(),
+				Optional:    true,
+				MaxItems:    1,
+			},
+			"qos_profile": {
+				Type:        schema.TypeList,
+				Description: "QoS profiles for this segment",
+				Elem:        getPolicySegmentQosProfilesSchema(),
+				Optional:    true,
+				MaxItems:    1,
+			},
+			"security_profile": {
+				Type:        schema.TypeList,
+				Description: "Security profiles for this segment",
+				Elem:        getPolicySegmentSecurityProfilesSchema(),
+				Optional:    true,
+				MaxItems:    1,
+			},
+		},
+	}
+}
+
+var allocateAddresses = []string{"IP_POOL", "MAC_POOL", "BOTH", "DHCP", "DHCPV6", "SLAAC", "NONE"}
+
+func getPolicySegmentPortSchema() *schema.Resource {
+	return &schema.Resource{
+		Schema: map[string]*schema.Schema{
+			"allocate_addresses": {
+				Type:         schema.TypeString,
+				Description:  "Indicate how IP will be allocated for the port. Alowed values are IP_POOL, MAC_POOL, BOTH, DHCP, DHCPV6, SLAAC, NONE",
+				Optional:     true,
+				ValidateFunc: validation.StringInSlice(allocateAddresses, false),
+			},
+			"app_id": {
+				Type:        schema.TypeString,
+				Description: "ID used to identify/look up a child attachment behind a parent attachment",
+				Optional:    true,
+			},
+			"evpn_vlans": {
+				Type:        schema.TypeList,
+				Description: "Evpn tenant VLAN IDs the Parent logical-port serves.",
+				Elem: &schema.Schema{
+					Type: schema.TypeString,
+				},
+				Optional: true,
+				MaxItems: 1000,
+			},
+			"hyperbus_mode": {
+				Type:         schema.TypeString,
+				Description:  "ID used to identify/look up a child attachment behind a parent attachment",
+				Optional:     true,
+				Default:      "DISABLE",
+				ValidateFunc: validation.StringInSlice([]string{"ENABLE", "DISABLE"}, false),
+			},
+			"type": {
+				Type:         schema.TypeString,
+				Description:  "Type of port attachment. PARENT type is automatically set if evpn_vlans or hyperbus_mode is configured. INDEPENDENT type is automatically set for ports that belong to Segment of type DVPortgroup. STATIC type is deprecated.",
+				Optional:     true,
+				ValidateFunc: validation.StringInSlice([]string{"PARENT", "CHILD", "INDEPENDENT", "STATIC"}, false),
+			},
+			"id": {
+				Type:        schema.TypeString,
+				Description: "VIF UUID on NSX Manager. If the attachement type is PARENT, this property is required.",
+				Optional:    true,
+			},
+			"traffic_tag": {
+				Type:        schema.TypeInt,
+				Description: "VIF UUID on NSX Manager. If the attachement type is PARENT, this property is required.",
+				Optional:    true,
+			},
+		},
+	}
+}
+
+func resourceNsxtPolicySegmentPortCreate(d *schema.ResourceData, m interface{}) error {
+	connector := getPolicyConnector(m)
+	context := getSessionContext(d, m)
+	id, err := getOrGenerateID2(d, m, resourceNsxtPolicySegmentPortExists(d, context, connector))
+	if err != nil {
+		return err
+	}
+
+	obj, err := policySegmentPortResourceToInfraStruct(context, id, d, false)
+	if err != nil {
+		return err
+	}
+
+	err = policyInfraPatch(context, obj, connector, false)
+	if err != nil {
+		return handleCreateError("SegmentPort", id, err)
+	}
+
+	d.SetId(id)
+	d.Set("nsx_id", id)
+
+	return resourceNsxtPolicySegmentPortRead(d, m)
+}
+
+func resourceNsxtPolicySegmentPortRead(d *schema.ResourceData, m interface{}) error {
+	connector := getPolicyConnector(m)
+
+	segmentPath := d.Get("segment_path").(string)
+	id := d.Id()
+	segPort, err := getSegmentPort(segmentPath, id, getSessionContext(d, m), connector)
+	if err != nil {
+		return fmt.Errorf("Error getting Segment Port : %v", err)
+	}
+
+	d.Set("display_name", segPort.DisplayName)
+	d.Set("description", segPort.Description)
+	setPolicyTagsInSchema(d, segPort.Tags)
+	d.Set("nsx_id", id)
+	d.Set("path", segPort.Path)
+	d.Set("revision", segPort.Revision)
+	err = nsxtPolicySegmentPortProfilesRead(d, m)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func resourceNsxtPolicySegmentPortUpdate(d *schema.ResourceData, m interface{}) error {
+	connector := getPolicyConnector(m)
+	context := getSessionContext(d, m)
+
+	id := d.Id()
+	if id == "" {
+		return fmt.Errorf("Error obtaining Segment ID")
+	}
+
+	obj, err := policySegmentPortResourceToInfraStruct(context, id, d, false)
+	if err != nil {
+		return err
+	}
+
+	err = policyInfraPatch(context, obj, connector, false)
+	if err != nil {
+		return handleCreateError("SegmentPort", id, err)
+	}
+
+	return resourceNsxtPolicySegmentPortRead(d, m)
+}
+
+func resourceNsxtPolicySegmentPortDelete(d *schema.ResourceData, m interface{}) error {
+	id := d.Id()
+	if id == "" {
+		return fmt.Errorf("Error obtaining Segment Port ID")
+	}
+	obj, err := policySegmentPortResourceToInfraStruct(getSessionContext(d, m), id, d, true)
+	if err != nil {
+		return err
+	}
+
+	err = policyInfraPatch(getSessionContext(d, m), obj, getPolicyConnector(m), false)
+	if err != nil {
+		return handleCreateError("SegmentPort", id, err)
+	}
+
+	return nil
+}
