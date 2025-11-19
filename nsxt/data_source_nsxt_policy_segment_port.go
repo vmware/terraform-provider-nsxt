e047c1fd1808dbacf16f2b125eca44b8e16333db
Arun Tony <aruntony005@gmail.com>
2025-11-20 02:00:09 +0530

    Adding id lookup to the segment port data source
    Signed-off-by: Arun Tony <aruntony005@gmail.com>

--- nsxt/data_source_nsxt_policy_segment_port.go
+++ nsxt/data_source_nsxt_policy_segment_port.go
@@ -47,6 +47,15 @@ func dataSourceNsxtPolicySegmentPortRead(d *schema.ResourceData, m interface{}) 
 		if len(segmentPortList) == 0 {
 			return fmt.Errorf("Segment Port with VIF attachment id %s not found", vifId)
 		}
+	} else if portName, exists := d.GetOkExists("id"); exists {
+		addQuery := fmt.Sprintf("id:%s", escapeSpecialCharacters(portName.(string)))
+		segmentPortList, err = listPolicyResources(connector, getSessionContext(d, m), "SegmentPort", &addQuery)
+		if err != nil {
+			return fmt.Errorf("Error getting the Segment Port list : %v", err)
+		}
+		if len(segmentPortList) == 0 {
+			return fmt.Errorf("Segment Port with id %s not found", portName)
+		}
 	} else if portName, exists := d.GetOkExists("display_name"); exists {
 		addQuery := fmt.Sprintf("display_name:%s", escapeSpecialCharacters(portName.(string)))
 		segmentPortList, err = listPolicyResources(connector, getSessionContext(d, m), "SegmentPort", &addQuery)
@@ -57,7 +66,7 @@ func dataSourceNsxtPolicySegmentPortRead(d *schema.ResourceData, m interface{}) 
 			return fmt.Errorf("Segment Port with display_name %s not found", portName)
 		}
 	} else {
-		return fmt.Errorf("Either vif_id or display_name should be set")
+		return fmt.Errorf("Atleast one of vif_id, display_name or id should be set")
 	}
 
 	dataValue, errors := converter.ConvertToGolang(segmentPortList[0], model.SegmentPortBindingType())
