1864f219029610e9e3c477160fab1c22b5d5307b
Kobi Samoray <kobi.samoray@broadcom.com>
2025-11-20 16:20:54 +0200

    Set upgrade target version correctly (#1890)
    Upgrade run resource target version is per state and shouldn't be set in the global scope
    
    Signed-off-by: Kobi Samoray <kobi.samoray@broadcom.com>

--- nsxt/resource_nsxt_upgrade_run.go
+++ nsxt/resource_nsxt_upgrade_run.go
@@ -957,6 +957,9 @@ func setUpgradeRunOutput(upgradeClientSet *upgradeClientSet, d *schema.ResourceD
 		if result.Details != nil {
 			elem["details"] = *result.Details
 		}
+		if result.TargetComponentVersion != nil {
+			elem["target_version"] = *result.TargetComponentVersion
+		}
 		groupStatusList, err := upgradeClientSet.GroupStatusClient.Getall(result.ComponentType, nil, nil, nil, nil, nil)
 		if err != nil {
 			return err
@@ -984,11 +987,6 @@ func resourceNsxtUpgradeRunRead(d *schema.ResourceData, m interface{}) error {
 	if err != nil {
 		return handleReadError(d, "NsxtUpgradeRun", id, err)
 	}
-	targetVersion, err := getTargetVersion(m)
-	if err != nil {
-		return handleReadError(d, "NsxtUpgradeRun", id, err)
-	}
-	d.Set("target_version", targetVersion)
 	return nil
 }
 
