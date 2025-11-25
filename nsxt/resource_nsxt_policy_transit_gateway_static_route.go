1f9d45e2d5459074aae1a0aba3b92c945ec6f0e8
Arun Tony <aruntony005@gmail.com>
2025-11-26 00:53:43 +0530

    Fixing lint
    Signed-off-by: Arun Tony <aruntony005@gmail.com>

--- nsxt/resource_nsxt_policy_transit_gateway_static_route.go
+++ nsxt/resource_nsxt_policy_transit_gateway_static_route.go
@@ -14,7 +14,7 @@ import (
 	"github.com/vmware/vsphere-automation-sdk-go/runtime/protocol/client"
 	"github.com/vmware/vsphere-automation-sdk-go/services/nsxt/model"
 
-	"github.com/vmware/terraform-provider-nsxt/api/orgs/projects/transit_gateways"
+	transitgateways "github.com/vmware/terraform-provider-nsxt/api/orgs/projects/transit_gateways"
 	utl "github.com/vmware/terraform-provider-nsxt/api/utl"
 	"github.com/vmware/terraform-provider-nsxt/nsxt/metadata"
 	"github.com/vmware/terraform-provider-nsxt/nsxt/util"
