3b08201b9ad3cd127f2147d13cab1500e3bbaaea
Pooja V <pooja.venkatesh@broadcom.com>
2026-01-12 12:37:32 +0530

    data source cache implementation

--- nsxt/data_source_nsxt_vpc_subnet.go
+++ nsxt/data_source_nsxt_vpc_subnet.go
@@ -9,6 +9,9 @@ import (
 
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"github.com/vmware/terraform-provider-nsxt/nsxt/util"
+	"github.com/vmware/vsphere-automation-sdk-go/runtime/bindings"
+	"github.com/vmware/vsphere-automation-sdk-go/runtime/data"
+	"github.com/vmware/vsphere-automation-sdk-go/services/nsxt/model"
 )
 
 func dataSourceNsxtVpcSubnet() *schema.Resource {
@@ -30,6 +33,33 @@ func dataSourceNsxtVpcSubnetRead(d *schema.ResourceData, m interface{}) error {
 		return fmt.Errorf("VPC Subnet data source requires NSX version 9.0.0 or higher")
 	}
 	connector := getPolicyConnector(m)
+	objID := d.Get("id").(string)
+
+	// Try serving from cache when the caller specified an ID.
+	// Data sources don't participate in refresh phase detection the same way resources do,
+	// so we call cache directly here while reusing the same cache storage and conversion.
+	if objID != "" && IsCacheEnabled() {
+		val, err := gcache.readCache(objID, "VpcSubnet", d, m, connector)
+		if err == nil {
+			converter := bindings.NewTypeConverter()
+			goVal, convErrs := converter.ConvertToGolang(val.(*data.StructValue), model.VpcSubnetBindingType())
+			if len(convErrs) == 0 {
+				obj, ok := goVal.(model.VpcSubnet)
+				if ok {
+					id := objID
+					if obj.Id != nil {
+						id = *obj.Id
+					}
+					d.SetId(id)
+					d.Set("id", id)
+					d.Set("display_name", obj.DisplayName)
+					d.Set("description", obj.Description)
+					d.Set("path", obj.Path)
+					return nil
+				}
+			}
+		}
+	}
 
 	_, err := policyDataSourceResourceReadWithValidation(d, connector, getSessionContext(d, m), "VpcSubnet", nil, false)
 	if err != nil {
