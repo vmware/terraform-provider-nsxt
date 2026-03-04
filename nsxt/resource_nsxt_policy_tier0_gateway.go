d6f20638b190ba912e19abaaf6f3d65a882157d7
aruntony005 <64554015+aruntony005@users.noreply.github.com>
2026-03-04 14:00:22 +0530

    Reverting the inter_sr_ibgp change (#1996)
    Signed-off-by: Arun Tony <aruntony005@gmail.com>

--- nsxt/resource_nsxt_policy_tier0_gateway.go
+++ nsxt/resource_nsxt_policy_tier0_gateway.go
@@ -887,7 +887,8 @@ func policyTier0GatewayResourceToInfraStruct(context utl.SessionContext, d *sche
 	if d.HasChange("bgp_config") && len(bgpConfig) > 0 && bgpConfig[0] != nil && !isGlobalManager {
 		// For Global Manager BGP is defined as separate resource
 		bgpCfgMap := bgpConfig[0].(map[string]interface{})
-		if bgpCfgMap["inter_sr_ibgp"] != nil {
+		if _, isSet := d.GetOkExists("bgp_config.0.inter_sr_ibgp"); !isSet {
+			log.Printf("[DEBUG] inter_sr_ibgp is not set, deleting from bgpCfgMap")
 			delete(bgpCfgMap, "inter_sr_ibgp")
 		}
 
