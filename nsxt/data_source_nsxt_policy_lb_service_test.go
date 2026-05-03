d9ea00326d387fed8ec1b9dc53d3fa0607a497ab
Kobi Samoray <kobi.samoray@broadcom.com>
2026-05-03 10:53:27 +0300

    Remove external dependency for LB service (#2052)
    Create a loadbalancer service within the test context instead of having
    an external dependency.
    
    Signed-off-by: Kobi Samoray <kobi.samoray@broadcom.com>

--- nsxt/data_source_nsxt_policy_lb_service_test.go
+++ nsxt/data_source_nsxt_policy_lb_service_test.go
@@ -9,23 +9,26 @@ import (
 	"testing"
 
 	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
+	"github.com/hashicorp/terraform-plugin-testing/terraform"
 )
 
 func TestAccDataSourceNsxtPolicyLBService(t *testing.T) {
-	name := getTestLBServiceName()
+	lbServiceName := getAccTestResourceName()
+	tier1Name := getAccTestResourceName()
+	tier0Name := getAccTestResourceName()
 	testResourceName := "data.nsxt_policy_lb_service.test"
 
-	resource.ParallelTest(t, resource.TestCase{
-		PreCheck: func() {
-			testAccPreCheck(t)
-			testAccEnvDefined(t, "NSXT_TEST_LB_SERVICE_NAME")
-		},
+	resource.Test(t, resource.TestCase{
+		PreCheck:  func() { testAccOnlyLocalManager(t); testAccPreCheck(t) },
 		Providers: testAccProviders,
+		CheckDestroy: func(state *terraform.State) error {
+			return testAccNsxtPolicyLBServiceCheckDestroy(state, lbServiceName)
+		},
 		Steps: []resource.TestStep{
 			{
-				Config: testAccNsxtPolicyLBServiceReadTemplate(name),
+				Config: testAccNsxtPolicyLBServiceDataSourceTemplate(lbServiceName, tier1Name, tier0Name),
 				Check: resource.ComposeTestCheckFunc(
-					resource.TestCheckResourceAttr(testResourceName, "display_name", name),
+					resource.TestCheckResourceAttr(testResourceName, "display_name", lbServiceName),
 					resource.TestCheckResourceAttrSet(testResourceName, "path"),
 				),
 			},
@@ -33,10 +36,35 @@ func TestAccDataSourceNsxtPolicyLBService(t *testing.T) {
 	})
 }
 
-func testAccNsxtPolicyLBServiceReadTemplate(name string) string {
+func testAccNsxtPolicyLBServiceDataSourceTemplate(lbServiceName, tier1Name, tier0Name string) string {
 	return fmt.Sprintf(`
+data "nsxt_policy_edge_cluster" "test" {
+  display_name = "%s"
+}
+
+resource "nsxt_policy_tier0_gateway" "test" {
+  display_name = "%s"
+}
+
+resource "nsxt_policy_tier1_gateway" "lb_svc_t1" {
+  display_name      = "%s"
+  edge_cluster_path = data.nsxt_policy_edge_cluster.test.path
+  tier0_path        = nsxt_policy_tier0_gateway.test.path
+}
+
+resource "nsxt_policy_lb_service" "created" {
+  display_name        = "%s"
+  connectivity_path   = nsxt_policy_tier1_gateway.lb_svc_t1.path
+}
+
 data "nsxt_policy_lb_service" "test" {
   display_name = "%s"
-}`, name)
 
+  depends_on = [nsxt_policy_lb_service.created]
+}
+
+data "nsxt_policy_realization_info" "realization_info" {
+  path = nsxt_policy_lb_service.created.path
+}
+`, getEdgeClusterName(), tier0Name, tier1Name, lbServiceName, lbServiceName)
 }
